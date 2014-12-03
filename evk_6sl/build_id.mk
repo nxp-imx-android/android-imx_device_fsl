cb4143bc17bf9e2b115792789ca3ab4ed667af08
guoyin.chen <guoyin.chen@freescale.com>
2014-12-03 15:24:07 +0800

    MA-5929 Update BUILD_ID for l5.0.0_1.0.0-alpha release
    Update BUILD_ID to 1.0.0-alpha-rc1
    Update BUILD_NUMBER to 20141213 as release date
    
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- evk_6sl/build_id.mk
+++ evk_6sl/build_id.mk
@@ -18,5 +18,5 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-export BUILD_ID=2.0.0-beta-rc4
-export BUILD_NUMBER=20140815
+export BUILD_ID=1.0.0-alpha-rc1
+export BUILD_NUMBER=20141213
