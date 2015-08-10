baef5633f9c6b69c3b358b709cb2d34257bcea01
guoyin.chen <guoyin.chen@freescale.com>
2015-08-10 15:39:15 +0800

    MA-6975-3 Fix the build error for fsl.git in m-preview
    Remove consumeir feature as no HAL built
    
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- imx6/sabresd_6dq.mk
+++ imx6/sabresd_6dq.mk
@@ -56,7 +56,6 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
 	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
 	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
-	frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml \
 	frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
 	device/fsl/sabresd_6dq/required_hardware.xml:system/etc/permissions/required_hardware.xml
 PRODUCT_PACKAGES += AudioRoute
