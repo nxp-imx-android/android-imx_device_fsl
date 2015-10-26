8f47549eebb63583219ba1b6cc91dcc3ed233f62
Liu Xiaowen <xiaowen.liu@freescale.com>
2015-10-26 11:05:57 +0800

    MA-7147-1 Enable single CSI camera support on Sabresd-6qp board.
    export init.i.MX6QP.rc to /root.
    move ov5640_mipi from back camera to front camera on 6qp board.
    move ov5640_camera from front camera on 6qp board.
    others board keep the same.
    
    Signed-off-by: Liu Xiaowen <xiaowen.liu@freescale.com>

--- imx6/sabresd_6dq.mk
+++ imx6/sabresd_6dq.mk
@@ -20,6 +20,7 @@ PRODUCT_COPY_FILES += \
 	device/fsl/sabresd_6dq/init.rc:root/init.freescale.rc \
         device/fsl/sabresd_6dq/init.i.MX6Q.rc:root/init.freescale.i.MX6Q.rc \
         device/fsl/sabresd_6dq/init.i.MX6DL.rc:root/init.freescale.i.MX6DL.rc \
+	device/fsl/sabresd_6dq/init.i.MX6QP.rc:root/init.freescale.i.MX6QP.rc \
 	device/fsl/sabresd_6dq/audio_policy.conf:system/etc/audio_policy.conf \
 	device/fsl/sabresd_6dq/audio_effects.conf:system/vendor/etc/audio_effects.conf
 
