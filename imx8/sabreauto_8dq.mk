55923c6f17c77d3108f2b53d96bc47b388d1c42d
Richard Liu <xuegang.liu@freescale.com>
2016-04-05 16:05:20 +0800

    MA-7879 add imx8dq device config
    imx8dv and imx8qm are 64bit platform, CPU use ARM cortex-a53.
    
    Change-Id: Ibfe7d64520061f579320f3f95818df4c9d7f252c
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/559
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    (cherry picked from commit e2764e57f674f73e84f558a5e1b21b314eee6119)
    Reviewed-on: http://androidsource.ap.freescale.net/project/647

---
+++ imx8/sabreauto_8dq.mk
@@ -0,0 +1,69 @@
+# This is a FSL Android Reference Design platform based on i.MX8QP ARD board
+# It will inherit from FSL core product which in turn inherit from Google generic
+
+$(call inherit-product, device/fsl/imx8/imx8.mk)
+
+ifneq ($(wildcard device/fsl/sabreauto_8dq/fstab_nand.freescale),)
+$(shell touch device/fsl/sabreauto_8dq/fstab_nand.freescale)
+endif
+
+ifneq ($(wildcard device/fsl/sabreauto_8dq/fstab.freescale),)
+$(shell touch device/fsl/sabreauto_8dq/fstab.freescale)
+endif
+
+# Overrides
+PRODUCT_NAME := sabreauto_8dq
+PRODUCT_DEVICE := sabreauto_8dq
+
+
+PRODUCT_COPY_FILES += \
+	device/fsl/sabreauto_8dq/init.rc:root/init.freescale.rc \
+	device/fsl/sabreauto_8dq/init.i.MX8DV.rc:root/init.freescale.i.MX8DV.rc \
+	device/fsl/sabreauto_8dq/init.i.MX8QM.rc:root/init.freescale.i.MX8QM.rc \
+	device/fsl/sabreauto_8dq/audio_policy.conf:system/etc/audio_policy.conf \
+	device/fsl/sabreauto_8dq/audio_policy_cts.conf:system/etc/audio_policy_cts.conf \
+	device/fsl/sabreauto_8dq/audio_effects.conf:system/vendor/etc/audio_effects.conf
+
+PRODUCT_COPY_FILES +=	\
+	external/linux-firmware-imx/firmware/vpu/vpu_fw_imx6d.bin:system/lib/firmware/vpu/vpu_fw_imx8dv.bin 	\
+	external/linux-firmware-imx/firmware/vpu/vpu_fw_imx6q.bin:system/lib/firmware/vpu/vpu_fw_imx8qm.bin
+# setup dm-verity configs.
+ifneq ($(BUILD_TARGET_FS),ubifs)
+PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/mmcblk2p5
+$(call inherit-product, build/target/product/verity.mk)
+endif
+# GPU files
+
+DEVICE_PACKAGE_OVERLAYS := device/fsl/sabreauto_6q/overlay
+
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_AAPT_CONFIG += xlarge large tvdpi hdpi xhdpi
+
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.audio.output.xml:system/etc/permissions/android.hardware.audio.output.xml \
+	frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+	frameworks/native/data/etc/android.hardware.screen.portrait.xml:system/etc/permissions/android.hardware.screen.portrait.xml \
+	frameworks/native/data/etc/android.hardware.screen.landscape.xml:system/etc/permissions/android.hardware.screen.landscape.xml \
+	frameworks/native/data/etc/android.software.app_widgets.xml:system/etc/permissions/android.software.app_widgets.xml \
+	frameworks/native/data/etc/android.software.voice_recognizers.xml:system/etc/permissions/android.software.voice_recognizers.xml \
+	frameworks/native/data/etc/android.software.backup.xml:system/etc/permissions/android.software.backup.xml \
+	frameworks/native/data/etc/android.software.print.xml:system/etc/permissions/android.software.print.xml \
+	frameworks/native/data/etc/android.software.device_admin.xml:system/etc/permissions/android.software.device_admin.xml \
+	frameworks/native/data/etc/android.software.managed_users.xml:system/etc/permissions/android.software.managed_users.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+	frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
+	device/fsl/sabreauto_8dq/required_hardware.xml:system/etc/permissions/required_hardware.xml \
+
+PRODUCT_PACKAGES += AudioRoute
---
+++ imx8/vendorsetup.sh
@@ -0,0 +1,3 @@
+
+add_lunch_combo sabreauto_8dq-eng
+add_lunch_combo sabreauto_8dq-user
