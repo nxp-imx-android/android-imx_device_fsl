56e9c03a7e30b2dc03d7ec486673d9ee70c4876f
zhang sanshan <b51434@freescale.com>
2016-05-20 10:18:24 +0800

    MA-8193 [Android_Imx8dv] Fix the compile error.
    1 enlarge BOARD_SYSTEMIMAGE_PARTITION_SIZE to 750M.
    2 change kernel config file to defconfig.
    
    Change-Id: Id557e050b6b2e0968666df17542137812a759476
    Signed-off-by: zhang sanshan <b51434@freescale.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/563
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>
    (cherry picked from commit 21ebecb9fd00191d0089391ac36d51b2e9d6caa2)
    Reviewed-on: http://androidsource.ap.freescale.net/project/667

--- imx8/soc/imx8dq.mk
+++ imx8/soc/imx8dq.mk
@@ -9,8 +9,7 @@ HAVE_FSL_IMX_GPU3D := true
 HAVE_FSL_IMX_IPU := false
 BOARD_KERNEL_BASE := 0x14000000
 LOAD_KERNEL_ENTRY := 0x10008000
-TARGET_KERNEL_DEFCONF := imx8dv_defconfig 
-#TARGET_KERNEL_DEFCONF := imx8dv_android_defconfig
+TARGET_KERNEL_DEFCONF := imx8_android_defconfig
 -include external/fsl_vpu_omx/codec_env.mk
 -include external/fsl_imx_omx/codec_env.mk
 TARGET_HAVE_IMX_GRALLOC := true
