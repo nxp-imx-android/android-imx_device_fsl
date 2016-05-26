9d4254f720b94acd0794dbea1597fcc17297e821
Xiaowen Liu <xiaowen.liu@nxp.com>
2016-05-26 15:23:18 -0400

    add back fsl-gpu.mk and remove duplicated rule.
    Signed-off-by: Xiaowen Liu <xiaowen.liu@nxp.com>

--- sabresd_6dq/BoardConfig.mk
+++ sabresd_6dq/BoardConfig.mk
@@ -5,6 +5,7 @@
 include device/fsl/imx6/soc/imx6dq.mk
 include device/fsl/sabresd_6dq/build_id.mk
 include device/fsl/imx6/BoardConfigCommon.mk
+include device/fsl-proprietary/gpu-viv/fsl-gpu.mk
 # sabresd_6dq default target for EXT4
 BUILD_TARGET_FS ?= ext4
 include device/fsl/imx6/imx6_target_fs.mk
