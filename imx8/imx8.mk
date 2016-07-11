3b9d910a3d1c90e9187668bf6ee43a7966f04bd5
Richard Liu <xuegang.liu@nxp.com>
2016-07-12 04:56:36 +0800

    MA-8370 [Android_IMX8] sabreauto: Bring up android on 8dv
    1. include core_64_bit.mk for 64bit build.
    2. no HWC on 8dv build.
    
    Change-Id: I50e88d4390576cfb8ee2e003f86bd6e43e2957be
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/621
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>
    (cherry picked from commit 6302fc9e8df325d3e323def32ded57fcd6c79bd2)
    Reviewed-on: http://androidsource.ap.freescale.net/project/672
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- imx8/imx8.mk
+++ imx8/imx8.mk
@@ -1,3 +1,4 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic.mk)
 $(call inherit-product, $(TOPDIR)frameworks/base/data/sounds/AllAudio.mk)
