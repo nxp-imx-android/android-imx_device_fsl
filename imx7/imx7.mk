a0b935423823cdf722194c27a53d8ef52bdaa27c
Tian Yang <yang.tian@nxp.com>
2016-09-05 12:54:54 +0800

    MA-8632 Use imx-firmware.git to hold wifi/bt firmware
    This patch fix softAP panic for BCM4339 in the workaround
    way by add apsta version firmware for BCM4339 softAP mode.
    
    Use fw_bcmdhd.bin instead of fw_bcmdhd_apsta.bin.
    
    Change-Id: Id650ae0cd1fa8fa51bc7dd679f0ac2593ce4601e
    Signed-off-by: Tian Yang <yang.tian@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/1037
    Reviewed-by: Wang Haoran <elven.wang@nxp.com>

--- imx7/imx7.mk
+++ imx7/imx7.mk
@@ -377,7 +377,7 @@ PRODUCT_COPY_FILES +=	\
 PRODUCT_COPY_FILES += \
 	external/imx-firmware/BCM4339/TypeZP/BCM4339_BT/Type_ZP.hcd:system/etc/firmware/bcm/Type_ZP.hcd 	\
 	external/imx-firmware/BCM4339/TypeZP/BCM4339_wifi/fw_bcmdhd.bin:system/etc/firmware/bcm/fw_bcmdhd.bin 	\
-	external/imx-firmware/BCM4339/TypeZP/BCM4339_wifi/fw_bcmdhd_apsta.bin:system/etc/firmware/bcm/fw_bcmdhd_apsta.bin 	\
+	external/imx-firmware/BCM4339/TypeZP/BCM4339_wifi/fw_bcmdhd.bin:system/etc/firmware/bcm/fw_bcmdhd_apsta.bin 	\
 	external/imx-firmware/brcm/1BW_BCM43340/BCM43341B0.1BW.hcd:system/etc/firmware/bcm/1BW_BCM43340/BCM43341B0.1BW.hcd 	\
 	external/imx-firmware/brcm/1BW_BCM43340/bcmdhd.1BW.OOB.cal:system/etc/firmware/bcm/1BW_BCM43340/bbcmdhd.1BW.OOB.cal 	\
 	external/imx-firmware/brcm/1BW_BCM43340/bcmdhd.1BW.SDIO.cal:system/etc/firmware/bcm/1BW_BCM43340/bcmdhd.1BW.SDIO.cal 	\
