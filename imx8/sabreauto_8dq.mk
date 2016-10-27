f672030d6b4011c1bbe6018431cc5ccd3114b532
Richard Liu <xuegang.liu@nxp.com>
2016-10-27 09:55:42 +0800

    MA-8909 fix testVulkanHardwareFeatures CTS failure on 8dv
    Set the correct Vulkan version and level on 8dv board,
    else testVulkanHardwareFeatures CTS will report error.
    
    Change-Id: I3e77667a6f41913a3fc8acf54609eefc4f41b492
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/1243
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- imx8/sabreauto_8dq.mk
+++ imx8/sabreauto_8dq.mk
@@ -68,6 +68,8 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.sensor.ambient_temperature.xml:system/etc/permissions/android.hardware.sensor.ambient_temperature.xml \
 	frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
 	frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
+	frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version-1_0_3.xml \
+	frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level-0.xml \
 	device/fsl/sabreauto_8dq/required_hardware.xml:system/etc/permissions/required_hardware.xml \
 
 PRODUCT_COPY_FILES += \
