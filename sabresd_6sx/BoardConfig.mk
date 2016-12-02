f410889263669bf849a70b26aa46736725da09db
Xiaowen Liu <xiaowen.liu@nxp.com>
2016-12-02 13:58:39 -0500

    MA-9081 Reduce vmalloc size to enlarge low memory space.
    The low memory space is limited by vmalloc size as they share kernel space.
    Reduce vmalloc size to enlarge low memory space.
    
    Change-Id: Id4db659695486638bcd460c2a17ec4c830c28961
    Signed-off-by: Xiaowen Liu <xiaowen.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/1364
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- sabresd_6sx/BoardConfig.mk
+++ sabresd_6sx/BoardConfig.mk
@@ -98,7 +98,7 @@ $(error "TARGET_USERIMAGES_USE_UBIFS and TARGET_USERIMAGES_USE_EXT4 config open 
 endif
 endif
 
-BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init androidboot.console=ttymxc0 consoleblank=0 androidboot.hardware=freescale vmalloc=256M cma=384M 
+BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init androidboot.console=ttymxc0 consoleblank=0 androidboot.hardware=freescale vmalloc=128M cma=384M
 
 ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
 #UBI boot command line.
