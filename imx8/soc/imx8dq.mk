f4e35269d7f56127716e2f3bbae42a99403e7cfd
ivan.liu <xiaowen.liu@nxp.com>
2017-05-11 15:50:30 +0800

    MA-9562 configure Gralloc1&HWC2.0 version.
    set Gralloc version to v3.
    set HWComposer vertion to v2.0
    and set TARGET_USES_HWC2 to true to enable HWComposer2.0 interface.
    
    Change-Id: I81038deedc72fd79b129fce1a2fc7ddf3454b2a0
    Signed-off-by: ivan.liu <xiaowen.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/2048
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- imx8/soc/imx8dq.mk
+++ imx8/soc/imx8dq.mk
@@ -12,9 +12,11 @@ BOARD_KERNEL_BASE := 0x80000000
 TARGET_KERNEL_DEFCONF := imx8_android_defconfig
 -include external/fsl_vpu_omx/codec_env.mk
 -include external/fsl_imx_omx/codec_env.mk
-TARGET_GRALLOC_VERSION := v2
+TARGET_GRALLOC_VERSION := v3
 TARGET_HIGH_PERFORMANCE := true
-TARGET_HWCOMPOSER_VERSION = v1.3
+# HWComposer version depends on this.
+TARGET_USES_HWC2 := true
+TARGET_HWCOMPOSER_VERSION := v2.0
 TARGET_HAVE_VIV_HWCOMPOSER = false
 TARGET_FSL_IMX_2D := DPU2D
 USE_OPENGL_RENDERER := true
