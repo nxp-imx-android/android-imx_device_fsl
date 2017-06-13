1b1f4aba79765665bb9ef2f6c2117de7f440c18b
sanshan zhang <sanshan.zhang@nxp.com>
2017-06-13 15:47:30 +0800

    MA-9721 change the firmware for imx8qm&imx8qxp
    add file scfw_tcm_8qm.bin and scfw_tcm_8qxp.bin
    
    Change-Id: I4c1542d36b0e6c8ba3e5f029f0e87cb2d70f58a2
    Signed-off-by: sanshan zhang <sanshan.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/2095
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-by: zhang bo <bo.zhang@nxp.com>

--- arm2_8qm/AndroidUboot.mk
+++ arm2_8qm/AndroidUboot.mk
@@ -3,7 +3,7 @@ MAKE += SHELL=/bin/bash
 
 define build_uboot
 	cp  bootable/bootloader/uboot-imx/u-boot.$(strip $(1)) external/imx-mkimage/iMX8QM/u-boot.bin; \
-	cp  external/linux-firmware-imx/firmware/scfw_tcm/scfw_tcm.bin external/imx-mkimage/iMX8QM/scfw_tcm.bin; \
+	cp  external/linux-firmware-imx/firmware/scfw_tcm/scfw_tcm_8qm.bin external/imx-mkimage/iMX8QM/scfw_tcm.bin; \
 	$(MAKE) -C external/imx-mkimage/iMX8QM/ clean; \
 	$(MAKE) -C external/imx-mkimage/iMX8QM/ flash; \
 	cp external/imx-mkimage/iMX8QM/flash.bin $(PRODUCT_OUT)/u-boot-$(strip $(2)).imx;
--- arm2_8qxp/AndroidUboot.mk
+++ arm2_8qxp/AndroidUboot.mk
@@ -3,7 +3,7 @@ MAKE += SHELL=/bin/bash
 
 define build_uboot
 	cp  bootable/bootloader/uboot-imx/u-boot.$(strip $(1)) external/imx-mkimage/iMX8QX/u-boot.bin; \
-	cp  external/linux-firmware-imx/firmware/scfw_tcm/scfw_tcm.bin external/imx-mkimage/iMX8QX/scfw_tcm.bin; \
+	cp  external/linux-firmware-imx/firmware/scfw_tcm/scfw_tcm_8qxp.bin external/imx-mkimage/iMX8QX/scfw_tcm.bin; \
 	$(MAKE) -C external/imx-mkimage/iMX8QX/ clean; \
 	$(MAKE) -C external/imx-mkimage/iMX8QX/ flash; \
 	cp external/imx-mkimage/iMX8QX/flash.bin $(PRODUCT_OUT)/u-boot-$(strip $(2)).imx;
