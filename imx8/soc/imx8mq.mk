59f44c4cbefe952c5d96204beaaa8c332aae6324
Richard Liu <xuegang.liu@nxp.com>
2017-06-20 16:45:43 +0800

    MA-9677-1 Update Gralloc and HWC version on imx8qm&imx8qxp
    Gralloc use v3 and HWC use v2.0 on imx8qm&imx8qxp.
    
    Change-Id: Id6170b9c1b864d43a8032fc0ff0839569360530a
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>

--- imx8/soc/imx8mq.mk
+++ imx8/soc/imx8mq.mk
@@ -12,9 +12,10 @@ BOARD_KERNEL_BASE := 0x80000000
 TARGET_KERNEL_DEFCONF := android_defconfig
 -include external/fsl_vpu_omx/codec_env.mk
 -include external/fsl_imx_omx/codec_env.mk
-TARGET_GRALLOC_VERSION := v2
+TARGET_GRALLOC_VERSION := v3
 TARGET_HIGH_PERFORMANCE := true
-TARGET_HWCOMPOSER_VERSION = v1.3
+TARGET_USES_HWC2 := true
+TARGET_HWCOMPOSER_VERSION := v2.0
 TARGET_HAVE_VIV_HWCOMPOSER = false
 USE_OPENGL_RENDERER := true
 TARGET_CPU_SMP := true
