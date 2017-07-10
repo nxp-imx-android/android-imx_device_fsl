4f364e794bbf3dd70c8ed68f8abdc473be62d152
zhang sanshan <sanshan.zhang@nxp.com>
2017-07-10 10:32:54 +0800

    MA-9870 [Android] imx8mq: switched to use imx-mkimage to pack various
    add "make flash_spl_uboot" for imx8mq.
    
    Change-Id: I174a89ec40806bdd78abe95967be8916d334cdc8
    Signed-off-by: zhang sanshan <sanshan.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/2358
    Reviewed-by: zhang bo <bo.zhang@nxp.com>
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    (cherry picked from commit 279413497d8e160ee8c5c62f428dedeae8236a38)
    Reviewed-on: http://androidsource.ap.freescale.net/project/2363

--- evk_8mq/AndroidUboot.mk
+++ evk_8mq/AndroidUboot.mk
@@ -2,10 +2,11 @@
 MAKE += SHELL=/bin/bash
 
 define build_uboot
-	dd if=bootable/bootloader/uboot-imx/SPL of=bootable/bootloader/uboot-imx/uboot.tmp; \
-	dd if=bootable/bootloader/uboot-imx/u-boot.img of=bootable/bootloader/uboot-imx/uboot.tmp bs=1k seek=351; \
-	cp bootable/bootloader/uboot-imx/uboot.tmp $(PRODUCT_OUT)/u-boot-$(strip $(2)).imx; \
-	rm bootable/bootloader/uboot-imx/uboot.tmp;
+	cp bootable/bootloader/uboot-imx/u-boot.$(strip $(1)) external/imx-mkimage/iMX8M/u-boot.bin; \
+	cp bootable/bootloader/uboot-imx/spl/u-boot-spl.bin  external/imx-mkimage/iMX8M/.; \
+	$(MAKE) -C external/imx-mkimage/iMX8M/ clean; \
+	$(MAKE) -C external/imx-mkimage/iMX8M/ flash_spl_uboot; \
+	cp external/imx-mkimage/iMX8M/flash.bin $(PRODUCT_OUT)/u-boot-$(strip $(2)).imx;
 endef
 
 
