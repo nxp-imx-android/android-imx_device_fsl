cfb9643271d2777125490c0a06b4a79d1e4eff5d
zhang sanshan <sanshan.zhang@nxp.com>
2017-07-14 15:41:22 +0800

    MA-9917 [Android] update build id
    change build id to rc3.
    
    Change-Id: I15c444ca26dbcc8051276db9fcb8d7ad64f661fc
    Signed-off-by: zhang sanshan <sanshan.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/2414
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- sabresd_6sx/build_id.mk
+++ sabresd_6sx/build_id.mk
@@ -18,5 +18,5 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-export BUILD_ID=2.0.0-rfp-rc2
-export BUILD_NUMBER=20170630
+export BUILD_ID=2.0.0-rfp-r3
+export BUILD_NUMBER=20170818
