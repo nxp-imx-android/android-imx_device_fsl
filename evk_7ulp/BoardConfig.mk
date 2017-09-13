143799f7f5acfffb4082193c1b9009baca3b0437
Zhang Bo <bo.zhang@nxp.com>
2017-09-13 15:33:44 +0800

    MA-10238[Android-MX7ULP-EVK] cannot boot android because of surfaceflinger crash
    Add graphic related package
    Modify the wrong path of media policy in evk_7ulp/BoardConfig.mk
    
    Change-Id: I5433d8590bfdac8704b68bf2a553d7013c3c6226
    Signed-off-by: Zhang Bo <bo.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/2846
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- evk_7ulp/BoardConfig.mk
+++ evk_7ulp/BoardConfig.mk
@@ -96,7 +96,7 @@ ADDITION_BPT_PARTITION = partition-table-14GB:device/fsl/common/partition/device
 
 # Vendor seccomp policy files for media components:
 PRODUCT_COPY_FILES += \
-       device/fsl/sabresd_6dq/seccomp/mediacodec-seccomp.policy:vendor/etc/seccomp_policy/mediacodec.policy \
-       device/fsl/sabresd_6dq/seccomp/mediaextractor-seccomp.policy:vendor/etc/seccomp_policy/mediaextractor.policy
+       device/fsl/evk_7ulp/seccomp/mediacodec-seccomp.policy:vendor/etc/seccomp_policy/mediacodec.policy \
+       device/fsl/evk_7ulp/seccomp/mediaextractor-seccomp.policy:vendor/etc/seccomp_policy/mediaextractor.policy
 
 TARGET_BOARD_KERNEL_HEADERS := device/fsl/common/kernel-headers
--- imx7/evk_7ulp.mk
+++ imx7/evk_7ulp.mk
@@ -90,6 +90,15 @@ PRODUCT_PACKAGES += \
     libVSC \
     libg2d \
     libgpuhelper
+# HWC2 HAL
+PRODUCT_PACKAGES += \
+	android.hardware.graphics.composer@2.1-impl
+
+# Gralloc HAL
+PRODUCT_PACKAGES += \
+	android.hardware.graphics.mapper@2.0-impl \
+	android.hardware.graphics.allocator@2.0-impl \
+	android.hardware.graphics.allocator@2.0-service
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
