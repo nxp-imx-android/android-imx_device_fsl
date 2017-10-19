01dfabc8f10bfd3d8f4641b98761bbb71e524c1a
zhang sanshan <sanshan.zhang@nxp.com>
2017-10-19 13:53:51 +0800

    MA-10482-3 [Android] enable FIT for android
    change the uboot process according BSP after enable FIT.
    
    Change-Id: I6658eb4e15362dac11259a87e83073ddf29b8a0b
    Signed-off-by: zhang sanshan <sanshan.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/3100
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- evk_8mq/AndroidUboot.mk
+++ evk_8mq/AndroidUboot.mk
@@ -2,8 +2,10 @@
 MAKE += SHELL=/bin/bash
 
 define build_uboot
-	cp out/target/product/evk_8mq/obj/BOOTLOADER_OBJ/u-boot.$(strip $(1)) external/imx-mkimage/iMX8M/u-boot.bin; \
+	cp out/target/product/evk_8mq/obj/BOOTLOADER_OBJ/u-boot-nodtb.$(strip $(1)) external/imx-mkimage/iMX8M/.; \
 	cp out/target/product/evk_8mq/obj/BOOTLOADER_OBJ/spl/u-boot-spl.bin  external/imx-mkimage/iMX8M/.; \
+	cp out/target/product/evk_8mq/obj/BOOTLOADER_OBJ/tools/mkimage  external/imx-mkimage/iMX8M/mkimage_uboot; \
+	cp out/target/product/evk_8mq/obj/BOOTLOADER_OBJ/arch/arm/dts/fsl-imx8mq-evk.dtb  external/imx-mkimage/iMX8M/.; \
 	cp device/fsl-proprietary/uboot-firmware/imx8m/hdmi_imx8m.bin external/imx-mkimage/iMX8M/.; \
 	cp device/fsl-proprietary/uboot-firmware/imx8m/lpddr4_pmu_train_imem.bin external/imx-mkimage/iMX8M/.; \
 	cp device/fsl-proprietary/uboot-firmware/imx8m/lpddr4_pmu_train_dmem.bin external/imx-mkimage/iMX8M/.; \
