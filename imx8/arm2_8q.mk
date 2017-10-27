89e81c284d4258adbb4f24baa0483ca081d63ea2
Richard Liu <xuegang.liu@nxp.com>
2017-10-27 09:25:12 +0800

    MA-10546 Fix VtsHalGraphicsMapperV2_0Target VTS can't run issue
    VtsHalGraphicsMapperV2_0Target VTS can't run and it block VTS test with latest VTS test suite,
    it is due to allocator not work in hwbinder mode. According to Google doc, only graphics.mapper
    and renderscript should work in passthrough mode, other hal all should work in hwbinder mode.
    
    Previous allocator hal work in passthrough mode due to GPU don't support fd and there is crash
    when enable hwbinder, now GPU 6.2.4 integrate a workaround patch to support fd and hwbinder is
    supported.
    
    Here change allocator and sensor hal to hwbinder mode, keep graphics.composer hal as passthrough
    because we don't enable hwcomposer composition for surfaceflinger and others also use passthrough
    hwcomposer.
    
    Change-Id: Ie963a4a423b219085ed5dbefc66ac53dd09add0c
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/3179
    Reviewed-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- imx8/arm2_8q.mk
+++ imx8/arm2_8q.mk
@@ -122,6 +122,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
     android.hardware.power@1.0-impl \
     android.hardware.power@1.0-service \
     android.hardware.light@2.0-impl \
