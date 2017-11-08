39d6732fcd93b5146a5603ff7b5218c32e36b6e3
zhang sanshan <sanshan.zhang@nxp.com>
2017-11-08 20:26:33 +0800

    MA-10598-1 [Android] hdmi firmware have been change to signed_hdmi_imx8m.bin
    hdmi_imx8m.bin change to igned_hdmi_imx8m.bin
    
    Change-Id: Ie3f58cc9a8878dbecbbf2029574b2ebbecc6d676
    Signed-off-by: zhang sanshan <sanshan.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/3293
    Reviewed-by: ji.luo <ji.luo@nxp.com>

--- evk_8mq/AndroidUboot.mk
+++ evk_8mq/AndroidUboot.mk
@@ -6,7 +6,7 @@ define build_uboot
 	cp out/target/product/evk_8mq/obj/BOOTLOADER_OBJ/spl/u-boot-spl.bin  external/imx-mkimage/iMX8M/.; \
 	cp out/target/product/evk_8mq/obj/BOOTLOADER_OBJ/tools/mkimage  external/imx-mkimage/iMX8M/mkimage_uboot; \
 	cp out/target/product/evk_8mq/obj/BOOTLOADER_OBJ/arch/arm/dts/fsl-imx8mq-evk.dtb  external/imx-mkimage/iMX8M/.; \
-	cp device/fsl-proprietary/uboot-firmware/imx8m/hdmi_imx8m.bin external/imx-mkimage/iMX8M/.; \
+	cp device/fsl-proprietary/uboot-firmware/imx8m/signed_hdmi_imx8m.bin  external/imx-mkimage/iMX8M/.; \
 	cp device/fsl-proprietary/uboot-firmware/imx8m/lpddr4_pmu_train_1d_dmem.bin external/imx-mkimage/iMX8M/.; \
 	cp device/fsl-proprietary/uboot-firmware/imx8m/lpddr4_pmu_train_1d_imem.bin external/imx-mkimage/iMX8M/.; \
 	cp device/fsl-proprietary/uboot-firmware/imx8m/lpddr4_pmu_train_2d_dmem.bin external/imx-mkimage/iMX8M/.; \
