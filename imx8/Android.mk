f291fe3bdd9f5a4cfd994dc518ed5607a7db8a64
zhang sanshan <sanshan.zhang@nxp.com>
2017-11-08 17:37:18 +0800

    MA-10596 [Android] add option -p when mkdir
    make sure we can mkdir hw even system/lib64 do not exist before.
    
    Change-Id: I13d608182e1dfc4e2674d2d2cd9a2accae13bf0e
    Signed-off-by: zhang sanshan <sanshan.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/3289
    Reviewed-by: zhang bo <bo.zhang@nxp.com>

--- imx8/Android.mk
+++ imx8/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE := IMXLinks
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_POST_INSTALL_CMD := \
-    mkdir $(PRODUCT_OUT)/system/lib64/hw; \
+    mkdir -p $(PRODUCT_OUT)/system/lib64/hw; \
     ln -sf /system/lib64/hw/bootctrl.avb.so $(PRODUCT_OUT)/system/lib64/hw/bootctrl.default.so
 
 include $(BUILD_PHONY_PACKAGE)
