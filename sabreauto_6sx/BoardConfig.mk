d614bb73c37ccb7bdf4b3dbc294620d1066649c5
ivan.liu <xiaowen.liu@nxp.com>
2017-11-16 16:36:10 +0800

    MA-10634 Reduce GPU reserved memory after memory allocation switch to ION.
    Reduce GPU reserved memory to 32M on SD board, 64M on AI board.
    
    Change-Id: Iaac2e3a23bc4f5339966de4063cfc8fd0bf627ac
    Signed-off-by: ivan.liu <xiaowen.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/3351
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- sabreauto_6sx/BoardConfig.mk
+++ sabreauto_6sx/BoardConfig.mk
@@ -64,7 +64,7 @@ TARGET_SELECT_KEY := 28
 # we don't support sparse image.
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
 DM_VERITY_RUNTIME_CONFIG := true
-BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init androidboot.console=ttymxc0 consoleblank=0 androidboot.hardware=freescale vmalloc=128M cma=512M galcore.contiguousSize=201326592
+BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init androidboot.console=ttymxc0 consoleblank=0 androidboot.hardware=freescale vmalloc=128M cma=512M galcore.contiguousSize=67108864
 
 ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
 #UBI boot command line.
--- sabresd_6sx/BoardConfig.mk
+++ sabresd_6sx/BoardConfig.mk
@@ -83,7 +83,7 @@ $(error "TARGET_USERIMAGES_USE_UBIFS and TARGET_USERIMAGES_USE_EXT4 config open 
 endif
 endif
 
-BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init androidboot.console=ttymxc0 consoleblank=0 androidboot.hardware=freescale vmalloc=128M cma=448M
+BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init androidboot.console=ttymxc0 consoleblank=0 androidboot.hardware=freescale vmalloc=128M cma=448M galcore.contiguousSize=33554432
 
 ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
 #UBI boot command line.
