3711f502923c63dd3ebb2dd40b88c25f0433dbe3
Wenbo.Zhu <wenbo.zhu_1@nxp.com>
2017-11-17 17:21:29 +0800

    MA-10668 [Android] update build id.
    Change build id to rc2 and change build number to 20180115.
    
    Change-Id: I5435f10df8d7f63c6306533898dd51865357aac6
    Signed-off-by: Wenbo.Zhu <wenbo.zhu_1@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/3367
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>

--- sabresd_6dq/build_id.mk
+++ sabresd_6dq/build_id.mk
@@ -18,5 +18,5 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-export BUILD_ID=1.0.0-rfp-rc1
-export BUILD_NUMBER=20171124
+export BUILD_ID=1.0.0-rfp-rc2
+export BUILD_NUMBER=20180115
