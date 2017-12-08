15675aa445026adb259ff129e1eeb20dac86a078
ivan.liu <xiaowen.liu@nxp.com>
2017-12-08 10:51:40 +0800

    MA-10875 Update video mode set for evk_8mq platform.
    Remove unused video set video=imxdpufb1:off video=imxdpufb2:off
    and video=imxdpufb3:off video=imxdpufb4:off.
    Add new video mode set and androidboot.gui_resolution set.
    
    Change-Id: Ie7cab081124963b7ccb3f861d20bb87843127a4e
    Signed-off-by: ivan.liu <xiaowen.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/3528
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- evk_8mq/BoardConfig.mk
+++ evk_8mq/BoardConfig.mk
@@ -92,7 +92,7 @@ BOARD_AVB_ENABLE := true
 # define frame buffer count
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 
-BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 earlycon=imxuart,0x30860000,115200 init=/init video=imxdpufb1:off video=imxdpufb2:off video=imxdpufb3:off video=imxdpufb4:off video=HDMI-A-1:1920x1080-32@60 androidboot.console=ttymxc0 consoleblank=0 androidboot.hardware=freescale cma=800M androidboot.gui_resolution=1080p
+BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 earlycon=imxuart,0x30860000,115200 init=/init video=HDMI-A-1:1920x1080-32@60 androidboot.console=ttymxc0 consoleblank=0 androidboot.hardware=freescale cma=800M
 
 BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/fsl/evk_8mq/bluetooth
