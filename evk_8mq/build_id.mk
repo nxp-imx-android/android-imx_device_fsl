dd117ed1adcc97c3a17d8a43dc22163d455912e9
zhang sanshan <sanshan.zhang@nxp.com>
2017-12-26 10:25:50 +0800

    MA-10893 [Android_8M-EVK]The build number for the PRC-RC1 image is wrong
    update build num.
    
    Change-Id: I54e69e42a491c2ee48af3febf063c030dca10553
    Signed-off-by: zhang sanshan <sanshan.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/3645
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- evk_8mq/build_id.mk
+++ evk_8mq/build_id.mk
@@ -18,5 +18,5 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-export BUILD_ID=1.1.0-ear-rc1
-export BUILD_NUMBER=20171013
+export BUILD_ID=1.3.0-prc-rc2
+export BUILD_NUMBER=20180110
