1bbdf0352a00331ae8c14c25ec1b99db29063bfb
yang.tian <yang.tian@nxp.com>
2018-01-09 17:05:36 +0800

    MA-11061 Put qcom wifi and bt firmware git in external
    Copy wifi and bluetooth firmware from external firmware git.
    
    Change-Id: Ied69f9df4e4056eb610e558762adf6091b017f48
    Signed-off-by: Tian Yang <yang.tian@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/3749
    Reviewed-by: Wang Haoran <elven.wang@nxp.com>

--- imx8/evk_8mq.mk
+++ imx8/evk_8mq.mk
@@ -138,11 +138,11 @@ PRODUCT_PACKAGES += \
 
 # Qcom WiFi Firmware
 PRODUCT_COPY_FILES += \
-    $(IMX_FIRMWARE_PATH)/linux-firmware/qca/nvm_00130302.bin:vendor/firmware/nvm_tlv_3.2.bin \
-    $(IMX_FIRMWARE_PATH)/linux-firmware/qca/rampatch_00130302.bin:vendor/firmware/rampatch_tlv_3.2.tlv \
-    $(IMX_FIRMWARE_PATH)/linux-firmware/ath10k/QCA6174/hw3.0/board.bin:vendor/firmware/ath10k/QCA6174/hw3.0/board.bin \
-    $(IMX_FIRMWARE_PATH)/linux-firmware/ath10k/QCA6174/hw3.0/board-2.bin:vendor/firmware/ath10k/QCA6174/hw3.0/board-2.bin \
-    $(IMX_FIRMWARE_PATH)/linux-firmware/ath10k/QCA6174/hw3.0/firmware-4.bin:vendor/firmware/ath10k/QCA6174/hw3.0/firmware-4.bin
+    external/linux-firmware/qca/nvm_00130302.bin:vendor/firmware/nvm_tlv_3.2.bin \
+    external/linux-firmware/qca/rampatch_00130302.bin:vendor/firmware/rampatch_tlv_3.2.tlv \
+    external/linux-firmware/ath10k/QCA6174/hw3.0/board.bin:vendor/firmware/ath10k/QCA6174/hw3.0/board.bin \
+    external/linux-firmware/ath10k/QCA6174/hw3.0/board-2.bin:vendor/firmware/ath10k/QCA6174/hw3.0/board-2.bin \
+    external/linux-firmware/ath10k/QCA6174/hw3.0/firmware-4.bin:vendor/firmware/ath10k/QCA6174/hw3.0/firmware-4.bin
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
