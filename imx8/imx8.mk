b8be4261455d653b85f686d7cda8cfdddf10526e
Wenbo.Zhu <wenbo.zhu_1@nxp.com>
2018-02-07 09:51:31 +0800

    MA-11345 [VTS]VtsHalCameraProviderV2_4Target not executed on imx8 platform.
    Add camera hidl support on imx8 platform.
    
    Change-Id: I5f05cd754c4c43c13d5718127a8795e45386ba2e
    Signed-off-by: Wenbo.Zhu <wenbo.zhu_1@nxp.com>

--- imx8/imx8.mk
+++ imx8/imx8.mk
@@ -94,6 +94,7 @@ PRODUCT_PACKAGES += \
        camera.device@1.0-impl          \
        camera.device@3.2-impl          \
        android.hardware.camera.provider@2.4-impl \
+	android.hardware.camera.provider@2.4-service \
        android.hardware.boot@1.0-impl \
        android.hardware.boot@1.0-service
 
