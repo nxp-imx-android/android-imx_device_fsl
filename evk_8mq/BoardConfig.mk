90c4101735331515021ca275b74e4e32cb0debbd
Antoine Bouyer <antoine.bouyer@nxp.com>
2018-03-12 10:07:27 +0100

    MMIOT-32 evk_8mq: change cma size for DRM build
    When Secure Data Path (SDP) is activated, decoded data are stored in
    the VPU ion buffer, instead of the cma HEAP. Thus memory allocated for
    VPU buffer can be deducted from the cma memory region.
    Moreover, crypted buffer size must also be deducted from CMA, otherwise
    cma allocation fails, and this is generating plenty of gpu allocation
    errors.
    
    Currently, 768MB are allocated for VPU decoded buffer, 32MB allocated
    for crypted buffer. This patch reduces cma to 736MB (instead of 1536MB
    as for other 8mq builds)
    
    Change-Id: Ica7e77b3cca3a4b696e56269897fe8f7f7c813d4
    Signed-off-by: Antoine Bouyer <antoine.bouyer@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/4270
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- evk_8mq/BoardConfig.mk
+++ evk_8mq/BoardConfig.mk
@@ -82,7 +82,13 @@ BOARD_AVB_ENABLE := true
 # define frame buffer count
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 5
 
-BOARD_KERNEL_CMDLINE += console=ttymxc0,115200 earlycon=imxuart,0x30860000,115200 init=/init video=HDMI-A-1:1920x1080-32@60 androidboot.console=ttymxc0 consoleblank=0 androidboot.hardware=freescale androidboot.fbTileSupport=enable cma=1536M androidboot.primary_display=imx-drm firmware_class.path=/vendor/firmware
+ifeq ($(PRODUCT_IMX_DRM),true)
+CMASIZE=736M
+else
+CMASIZE=1536M
+endif
+
+BOARD_KERNEL_CMDLINE += console=ttymxc0,115200 earlycon=imxuart,0x30860000,115200 init=/init video=HDMI-A-1:1920x1080-32@60 androidboot.console=ttymxc0 consoleblank=0 androidboot.hardware=freescale androidboot.fbTileSupport=enable cma=$(CMASIZE) androidboot.primary_display=imx-drm firmware_class.path=/vendor/firmware
 
 # Qcom 1CQ(QCA6174) BT
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/fsl/evk_8mq/bluetooth
