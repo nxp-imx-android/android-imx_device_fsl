4eb21d98964b38fe07994989bc8c07e4c923db4f
Zhang Bo <bo.zhang@nxp.com>
2018-03-19 17:54:43 +0800

    MA-11564-1[Android] change the i2c rate in dts to boot faster
    Use specific dtb for car image
    
    Change-Id: I8a362ad67501ef4f6ee1e5509906b8114247d9bc
    Signed-off-by: Zhang Bo <bo.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/4223
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>

--- mek_8q/BoardConfig.mk
+++ mek_8q/BoardConfig.mk
@@ -166,7 +166,7 @@ endif
 endif
 
 ifeq ($(PRODUCT_IMX_CAR),true)
-TARGET_BOARD_DTS_CONFIG := imx8qm:fsl-imx8qm-mek.dtb imx8qm-mipi:fsl-imx8qm-mek-dsi-adv7535.dtb imx8qm-hdmi:fsl-imx8qm-mek-hdmi.dtb imx8qxp:fsl-imx8qxp-mek-lvds0-it6263.dtb imx8qxp-mipi:fsl-imx8qxp-mek-dsi-adv7535.dtb
+TARGET_BOARD_DTS_CONFIG := imx8qm:fsl-imx8qm-mek-car.dtb imx8qxp:fsl-imx8qxp-mek-lvds0-it6263-car.dtb
 TARGET_BOOTLOADER_CONFIG := imx8qm:mx8qm_mek_androidauto_defconfig imx8qxp:mx8qxp_mek_androidauto_defconfig
 else
 TARGET_BOARD_DTS_CONFIG := imx8qm:fsl-imx8qm-mek.dtb imx8qm-dual:fsl-imx8qm-mek-it6263.dtb imx8qm-mipi:fsl-imx8qm-mek-dsi-adv7535.dtb imx8qm-hdmi:fsl-imx8qm-mek-hdmi.dtb imx8qxp:fsl-imx8qxp-mek-lvds0-it6263.dtb imx8qxp-dual:fsl-imx8qxp-mek-lvds0-lvds1-it6263.dtb imx8qxp-mipi:fsl-imx8qxp-mek-dsi-adv7535.dtb
