97fdc8084e78efd9c4a1e97c9d5f26e1efc9be88
Richard Liu <xuegang.liu@nxp.com>
2018-03-28 00:04:32 +0800

    MA-11625 CTS android.opengl.cts.OpenGlEsVersionTest#testExtensionPack fail
    GPU on 8mscale board does not support OpenGL ES 3.1 AEP feature,
    so need remove this feature configure on 8mscale build.
    
    Change-Id: Idc5913b353dc5a41e1204911a82b4d7c7758fdb1
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/4301
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- imx8/evk_8mq.mk
+++ imx8/evk_8mq.mk
@@ -66,7 +66,6 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.camera.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.xml \
 	frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
 	frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml \
-	frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
 	frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_0_3.xml \
 	frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-0.xml \
 	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
