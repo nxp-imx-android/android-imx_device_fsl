a35e5b3484c5a7a3132be37d676ccd48dc46894a
Jessie Hao <juan.hao@nxp.com>
2018-06-11 14:17:15 +0800

    MA-11991 evk_8mm use drm display,update primary_display bootargs.
    Update bootargs to androidboot.primary_display=imx-drm.
    
    Change-Id: I529668900dad6151a851378837c4fe7d0073b8e2
    Signed-off-by: Jessie Hao <juan.hao@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/4823
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- evk_8mm/BoardConfig.mk
+++ evk_8mm/BoardConfig.mk
@@ -97,7 +97,7 @@ CMASIZE=800M
 endif
 
 KERNEL_NAME := Image
-BOARD_KERNEL_CMDLINE := console=ttymxc1,115200 earlycon=ec_imx6q,0x30890000,115200 init=/init androidboot.console=ttymxc1 consoleblank=0 androidboot.hardware=freescale cma=$(CMASIZE) androidboot.primary_display=mxsfb-drm firmware_class.path=/vendor/firmware
+BOARD_KERNEL_CMDLINE := console=ttymxc1,115200 earlycon=ec_imx6q,0x30890000,115200 init=/init androidboot.console=ttymxc1 consoleblank=0 androidboot.hardware=freescale cma=$(CMASIZE) androidboot.primary_display=imx-drm firmware_class.path=/vendor/firmware
 
 ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
 ifeq ($(TARGET_USERIMAGES_USE_EXT4),true)
