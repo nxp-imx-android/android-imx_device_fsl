11670450fe036ea0516f20f04d1ea2cc5cd8856c
Jindong <jindong.yue@nxp.com>
2018-07-05 13:13:28 +0800

    MA-12152 p-preview-3: evk_8mm: use new kernel and uboot build rule
    Kernel: device/fsl/common/build/kernel.mk
    Uboot: device/fsl/common/build/uboot.mk
    
    Change-Id: Iea5d926c6d522b2295dd2d700432e73ffe749c47
    Signed-off-by: Jindong <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/5044
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>

--- evk_8mm/AndroidBoard.mk
+++ evk_8mm/AndroidBoard.mk
@@ -1,6 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(FSL_PROPRIETARY_PATH)/fsl-proprietary/media-profile/media-profile.mk
-include $(FSL_PROPRIETARY_PATH)/fsl-proprietary/sensor/fsl-sensor.mk
+include device/fsl/common/build/kernel.mk
+include device/fsl/common/build/uboot.mk
 include device/fsl/evk_8mm/AndroidUboot.mk
 include device/fsl/evk_8mm/AndroidTee.mk
+include $(FSL_PROPRIETARY_PATH)/fsl-proprietary/media-profile/media-profile.mk
+include $(FSL_PROPRIETARY_PATH)/fsl-proprietary/sensor/fsl-sensor.mk
