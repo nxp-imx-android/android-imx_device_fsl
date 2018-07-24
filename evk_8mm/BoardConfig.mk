e508ac1f7a2b2e02f15a21695a15d224eda64f52
Dandan Sun <dandan.sun@nxp.com>
2018-07-24 16:44:44 +0800

    MA-12226-1 Enable mipi screen on imx8mm-ev
    add fsl-imx8mm-evk-rm67191.dtb to enable mipi panel.
    
    Change-Id: Ie59add844685a3a6ccf7db0dca74a06eb8f5d424
    Signed-off-by: Dandan Sun <dandan.sun@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/5162
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>

--- evk_8mm/BoardConfig.mk
+++ evk_8mm/BoardConfig.mk
@@ -106,7 +106,7 @@ $(error "TARGET_USERIMAGES_USE_UBIFS and TARGET_USERIMAGES_USE_EXT4 config open 
 endif
 endif
 
-TARGET_BOARD_DTS_CONFIG ?= imx8mm:fsl-imx8mm-evk.dtb imx8mm-dsd:fsl-imx8mm-evk-ak4497.dtb
+TARGET_BOARD_DTS_CONFIG ?= imx8mm:fsl-imx8mm-evk.dtb imx8mm-mipi-panel:fsl-imx8mm-evk-rm67191.dtb imx8mm-dsd:fsl-imx8mm-evk-ak4497.dtb
 TARGET_BOOTLOADER_CONFIG := imx8mm:imx8mm_evk_android_defconfig
 TARGET_KERNEL_DEFCONF := android_defconfig
 TARGET_KERNEL_ADDITION_DEFCONF := android_addition_defconfig
