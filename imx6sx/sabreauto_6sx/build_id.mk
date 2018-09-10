f8239e0d8e4901f9f44fdf0c9a38cdb24296a22e
Jindong <jindong.yue@nxp.com>
2018-09-10 17:53:23 +0800

    MA-12643 add rc1 BUILD_ID and BUILD_NUMBER for all boards
    imx8m/8q:   1.0.0-beta-rc1
    imx6/7:     1.0.1-ga-rc1
    imx8q_car:  1.0.2-auto-alpha-rc1
    
    Change-Id: I929481ef504b5b66e7d5b38edc156cc052510061
    Signed-off-by: Jindong <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/5895
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- imx6sx/sabreauto_6sx/build_id.mk
+++ imx6sx/sabreauto_6sx/build_id.mk
@@ -18,5 +18,5 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-#export BUILD_ID=1.0.0-rfp-rc3
-#export BUILD_NUMBER=20180129
+export BUILD_ID=1.0.1-ga-rc1
+export BUILD_NUMBER=20181109
