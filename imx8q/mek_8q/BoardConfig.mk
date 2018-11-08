4634b6e631e47769529d67333fc0727d178de26e
Dandan Sun <dandan.sun@nxp.com>
2018-11-08 18:48:45 +0800

    MA-13380 Enable mipi panel display on MEK_8QM
    add imx8qm-mipi-panel:fsl-imx8qm-mek-dsi-rm67191.dtb
    add selinux lable for backlight sysnode.
    change the default path of backlight.
    
    Conflicts:
    	imx8m/evk_8mm/sepolicy/genfs_contexts
    	imx8q/mek_8q/sepolicy/genfs_contexts
    
    Change-Id: I501d3486c464a841d62ae49500e824b9ce1c8b62
    Signed-off-by: Dandan Sun <dandan.sun@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/6568
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- imx8q/mek_8q/BoardConfig.mk
+++ imx8q/mek_8q/BoardConfig.mk
@@ -166,7 +166,7 @@ ifeq ($(PRODUCT_IMX_CAR),true)
 TARGET_BOARD_DTS_CONFIG := imx8qm:fsl-imx8qm-mek-car.dtb imx8qm-xen:fsl-imx8qm-mek-domu-car.dtb imx8qxp:fsl-imx8qxp-mek-car.dtb
 TARGET_BOOTLOADER_CONFIG := imx8qm:imx8qm_mek_androidauto_trusty_defconfig imx8qm-xen:imx8qm_mek_androidauto_xen_defconfig imx8qxp:imx8qxp_mek_androidauto_trusty_defconfig
 else
-TARGET_BOARD_DTS_CONFIG := imx8qm:fsl-imx8qm-mek-mipi-two-ov5640.dtb imx8qm-hdmi:fsl-imx8qm-mek-hdmi.dtb imx8qxp:fsl-imx8qxp-mek-ov5640.dtb imx8qxp-ov5640mipi:fsl-imx8qxp-mek-mipi-ov5640.dtb
+TARGET_BOARD_DTS_CONFIG := imx8qm:fsl-imx8qm-mek-mipi-two-ov5640.dtb imx8qm-mipi-panel:fsl-imx8qm-mek-dsi-rm67191.dtb imx8qm-hdmi:fsl-imx8qm-mek-hdmi.dtb imx8qxp:fsl-imx8qxp-mek-ov5640.dtb imx8qxp-ov5640mipi:fsl-imx8qxp-mek-mipi-ov5640.dtb
 TARGET_BOOTLOADER_CONFIG := imx8qm:imx8qm_mek_android_defconfig imx8qxp:imx8qxp_mek_android_defconfig
 endif #PRODUCT_IMX_CAR
 
