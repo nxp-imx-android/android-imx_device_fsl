27eb48c140d410f623b721b8599a5882dfe54972
Jindong <jindong.yue@nxp.com>
2018-11-30 10:04:38 +0800

    MA-13372-1 [Android] update build id
    Update build id for below release:
    p9.0.0_1.0.0-ga-20181130
    p9.0.0_1.0.2-auto-beta-20181130
    
    Change-Id: I3902a433014561ddac32dea9d06ed95714fb329a
    Signed-off-by: Jindong <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/6746
    Reviewed-by: juan.hao <juan.hao@nxp.com>

--- imx8q/mek_8q/build_id_car.mk
+++ imx8q/mek_8q/build_id_car.mk
@@ -18,5 +18,5 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-export BUILD_ID=1.0.2-auto-beta-rc1
+export BUILD_ID=1.0.2-auto-beta-rc2
 export BUILD_NUMBER=20190114
