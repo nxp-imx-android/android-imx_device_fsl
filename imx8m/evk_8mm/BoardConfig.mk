26efc88fa26c970984d2891bb8faff4ad2b4c902
zhang sanshan <sanshan.zhang@nxp.com>
2018-12-03 10:21:51 +0800

    MA-13635 [Android] refine bootargs in android
    galcore.contiguousSize=32M is only needed in android auto.
    androidboot.console=ttyLP0 is appended in device tree to distinguish
    normal android and android auto in XEN. it's defined in board file
    in normal android.
    remove consoleblank which is not enable in imx8
    
    Change-Id: I49d9de58ac95ead0040b11b84ac74dd655ced85e
    Signed-off-by: zhang sanshan <sanshan.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/6781
    Reviewed-by: zhang bo <bo.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/7799
    Reviewed-by: ming.zhou <ming.zhou@nxp.com>

--- imx8m/evk_8mm/BoardConfig.mk
+++ imx8m/evk_8mm/BoardConfig.mk
@@ -104,7 +104,7 @@ NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 CMASIZE=800M
 
 KERNEL_NAME := Image
-BOARD_KERNEL_CMDLINE := init=/init androidboot.console=ttymxc1 consoleblank=0 androidboot.hardware=freescale cma=$(CMASIZE) androidboot.primary_display=imx-drm firmware_class.path=/vendor/firmware transparent_hugepage=never
+BOARD_KERNEL_CMDLINE := init=/init androidboot.console=ttymxc1 androidboot.hardware=freescale cma=$(CMASIZE) androidboot.primary_display=imx-drm firmware_class.path=/vendor/firmware transparent_hugepage=never
 
 ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
 ifeq ($(TARGET_USERIMAGES_USE_EXT4),true)
--- imx8m/evk_8mq/BoardConfig.mk
+++ imx8m/evk_8mq/BoardConfig.mk
@@ -105,7 +105,7 @@ CMASIZE=1280M
 endif
 
 KERNEL_NAME := Image
-BOARD_KERNEL_CMDLINE := init=/init androidboot.gui_resolution=1080p androidboot.console=ttymxc0 consoleblank=0 androidboot.hardware=freescale androidboot.fbTileSupport=enable cma=$(CMASIZE) androidboot.primary_display=imx-drm firmware_class.path=/vendor/firmware transparent_hugepage=never
+BOARD_KERNEL_CMDLINE := init=/init androidboot.gui_resolution=1080p androidboot.console=ttymxc0 androidboot.hardware=freescale androidboot.fbTileSupport=enable cma=$(CMASIZE) androidboot.primary_display=imx-drm firmware_class.path=/vendor/firmware transparent_hugepage=never
 
 # Qcom 1CQ(QCA6174) BT
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(IMX_DEVICE_PATH)/bluetooth
