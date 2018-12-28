a8ec2e15a998f9d88c70372e19c34161732c7511
Zhang Bo <bo.zhang@nxp.com>
2018-12-28 18:32:29 +0800

    MA-13795-1[Android] Initial MCU SDK source code for android automotive demo
    Build M4 binary for android auto image
    The MCU M4 SDK source code is in the vendor/nxp/mcu-sdk-auto and build folder
    is in $PRODUCT_OUT/obj/MCU_OBJ
    
    Change-Id: I3c202bc594f5eae380d39700da01e906ed3e2f6e
    Signed-off-by: Zhang Bo <bo.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/6994
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/7005

--- imx8q/mek_8q/AndroidUboot.mk
+++ imx8q/mek_8q/AndroidUboot.mk
@@ -2,6 +2,41 @@
 MAKE += SHELL=/bin/bash
 ATF_TOOLCHAIN_ABS := $(realpath prebuilts/gcc/$(HOST_PREBUILT_TAG)/aarch64/aarch64-linux-android-4.9/bin)
 ATF_CROSS_COMPILE := $(ATF_TOOLCHAIN_ABS)/aarch64-linux-androidkernel-
+MCU_SDK_IMX8QM_DEMO_PATH := $(IMX_MCU_SDK_PATH)/mcu-sdk-auto/SDK_MEK-MIMX8QM/boards/mekmimx8qm/demo_apps/rear_view_camera/cm4_core1/armgcc
+MCU_SDK_IMX8QM_CMAKE_FILE := ../../../../../../tools/cmake_toolchain_files/armgcc.cmake
+MCU_SDK_IMX8QX_DEMO_PATH := $(IMX_MCU_SDK_PATH)/mcu-sdk-auto/SDK_MEK-MIMX8QX/boards/mekmimx8qx/demo_apps/rear_view_camera/armgcc
+MCU_SDK_IMX8QX_CMAKE_FILE := ../../../../../tools/cmake_toolchain_files/armgcc.cmake
+
+UBOOT_M4_OUT := $(TARGET_OUT_INTERMEDIATES)/MCU_OBJ
+
+define build_M4_image
+	mkdir -p $(UBOOT_M4_OUT)/$2; \
+	cmake -DCMAKE_TOOLCHAIN_FILE="$4" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$3 -S $1 -B $(UBOOT_M4_OUT)/$2; \
+	$(MAKE) -C $(UBOOT_M4_OUT)/$2
+endef
+
+ifeq ($(PRODUCT_IMX_CAR_M4),true)
+ifeq ($(ARMGCC_DIR),)
+$(error please install arm-none-eabi-gcc toolchain and set the installed path to ARMGCC_DIR)
+endif
+
+$(UBOOT_M4_OUT):
+	rm -rf $@
+	mkdir -p $@
+
+UBOOT_M4_BIN: $(UBOOT_M4_OUT)
+	$(hide) echo "Building M4 image for UBoot ..."
+	cmake_version=$(shell cmake --version | head -n 1 | tr " " "\n" | tail -n 1); \
+	req_version="3.13.0"; \
+	if [ "$(shell echo "$$cmake_version $$req_version" | tr " " "\n" | sort -V | head -n 1)" != "$$req_version" ]; then \
+		echo "please upgrade cmake version to 3.13.0 or newer"; \
+		exit 1; \
+	fi; \
+	$(call build_M4_image,$(MCU_SDK_IMX8QM_DEMO_PATH),MIMX8QM,release,$(MCU_SDK_IMX8QM_CMAKE_FILE)); \
+	$(call build_M4_image,$(MCU_SDK_IMX8QX_DEMO_PATH),MIMX8QX,release,$(MCU_SDK_IMX8QX_CMAKE_FILE))
+else
+UBOOT_M4_BIN:
+endif # PRODUCT_IMX_CAR_M4
 
 define build_imx_uboot
 	if [ `echo $(2) | cut -d '-' -f1` == "imx8qm" ] && [ `echo $(2) | cut -d '-' -f2` != "xen" ]; then \
@@ -23,7 +58,7 @@ define build_imx_uboot
 		else \
 			if [ "$(PRODUCT_IMX_CAR_M4)" == "true" ] ; then \
 				FLASH_TARGET=`echo flash_b0_spl_container_m4_1_trusty`;  \
-				cp  $(FSL_PROPRIETARY_PATH)/fsl-proprietary/mcu-sdk/imx8q/imx8qm_m4_1_tcm_auto.bin $(IMX_MKIMAGE_PATH)/imx-mkimage/$$MKIMAGE_PLATFORM/m41_tcm.bin; \
+				cp  $(UBOOT_M4_OUT)/MIMX8QM/release/m4_image.bin $(IMX_MKIMAGE_PATH)/imx-mkimage/$$MKIMAGE_PLATFORM/m41_tcm.bin; \
 			else \
 				FLASH_TARGET=`echo flash_b0_spl_container_trusty`;  \
 			fi; \
@@ -47,7 +82,7 @@ define build_imx_uboot
 		fi; \
 		if [ "$(PRODUCT_IMX_CAR)" == "true" ] && [ `echo $(2) | rev | cut -d '-' -f1` != "uuu" ]; then \
 		    if [ "$(PRODUCT_IMX_CAR_M4)" == "true" ] ; then \
-				cp  $(FSL_PROPRIETARY_PATH)/fsl-proprietary/mcu-sdk/imx8q/imx8qx_m4_tcm_auto.bin $(IMX_MKIMAGE_PATH)/imx-mkimage/$$MKIMAGE_PLATFORM/CM4.bin; \
+				cp  $(UBOOT_M4_OUT)/MIMX8QX/release/rear_view_camera.bin $(IMX_MKIMAGE_PATH)/imx-mkimage/$$MKIMAGE_PLATFORM/CM4.bin; \
 			else \
 				cp  $(FSL_PROPRIETARY_PATH)/fsl-proprietary/mcu-sdk/imx8q/imx8qx_m4_default.bin $(IMX_MKIMAGE_PATH)/imx-mkimage/$$MKIMAGE_PLATFORM/CM4.bin; \
 			fi; \
