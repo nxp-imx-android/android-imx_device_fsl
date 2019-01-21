59d801f8252b7105767d800c4c83d842931df431
zhang sanshan <sanshan.zhang@nxp.com>
2019-01-21 09:45:05 +0800

    MA-13967-3 update kernel user space header file
    use common/tools/update_kerenl_header.sh to update
    
    Change-Id: Ic2b4a339e1897d11c86a5706cffb625cf3885e49
    Signed-off-by: zhang sanshan <sanshan.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/7192
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-by: zhang bo <bo.zhang@nxp.com>

--- common/kernel-headers/linux/dma-buf.h
+++ common/kernel-headers/linux/dma-buf.h
@@ -20,23 +20,18 @@
 #define _DMA_BUF_UAPI_H_
 #include <linux/types.h>
 struct dma_buf_sync {
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
   __u64 flags;
 };
 struct dma_buf_phys {
   __u64 phys;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 };
 #define DMA_BUF_SYNC_READ (1 << 0)
 #define DMA_BUF_SYNC_WRITE (2 << 0)
 #define DMA_BUF_SYNC_RW (DMA_BUF_SYNC_READ | DMA_BUF_SYNC_WRITE)
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #define DMA_BUF_SYNC_START (0 << 2)
 #define DMA_BUF_SYNC_END (1 << 2)
 #define DMA_BUF_SYNC_VALID_FLAGS_MASK (DMA_BUF_SYNC_RW | DMA_BUF_SYNC_END)
 #define DMA_BUF_BASE 'b'
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #define DMA_BUF_IOCTL_SYNC _IOW(DMA_BUF_BASE, 0, struct dma_buf_sync)
 #define DMA_BUF_IOCTL_PHYS _IOW(DMA_BUF_BASE, 1, struct dma_buf_phys)
 #endif
-
