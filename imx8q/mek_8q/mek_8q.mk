14c93a623509e24af155a8c1a8262640eaabf4f4
Richard Liu <xuegang.liu@nxp.com>
2019-01-28 23:54:02 +0800

    MA-14027 enable some core features on imx android build
    Below features are supported since android8.0, enable them
    on imx android build.
    Standard android build reference tablet_core_hardware.xml,
    car/car2 android build reference car_core_hardware.xml.
    
    <feature name="android.software.picture_in_picture" />
    <feature name="android.software.activities_on_secondary_displays" />
    <feature name="android.software.companion_device_setup" />
    <feature name="android.software.autofill" />
    
    Change-Id: If259b06625719e6b6df9e2b20165e15f47174c7c
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/7293
    Reviewed-by: Xiaowen Liu <xiaowen.liu@nxp.com>

--- imx8q/mek_8q/mek_8q.mk
+++ imx8q/mek_8q/mek_8q.mk
@@ -102,7 +102,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
     frameworks/native/data/etc/android.software.voice_recognizers.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.voice_recognizers.xml \
-    frameworks/native/data/etc/android.software.activities_on_secondary_displays.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.activities_on_secondary_displays.xml
+    frameworks/native/data/etc/android.software.activities_on_secondary_displays.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.activities_on_secondary_displays.xml \
+    frameworks/native/data/etc/android.software.picture_in_picture.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.picture_in_picture.xml
 
 ifneq ($(PRODUCT_IMX_CAR),true)
 PRODUCT_COPY_FILES += \
