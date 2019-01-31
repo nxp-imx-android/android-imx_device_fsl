ed844aa732e71de43405cabc8ee1b6e988356581
yang.tian <yang.tian@nxp.com>
2019-01-31 16:24:59 +0800

    MA-13945-3 Set 8mm default support qca wifi
    Since for 8mm OTA, it's target is LPDDR4 board with
    QCA 1PJ wifi module, set 8mm default bootargs support
    LPDDR4 board, which can get LPDDR4 boot up with wifi
    available after OTA upgrade.
    
    For DDR4 boards with BCM 1MW, need to set bootargs to
    "androidboot.wifivendor=bcm".
    
    Change-Id: Ic9dc57d9f3cf567b8340c1d91668f25f0f01b8d2
    Signed-off-by: yang.tian <yang.tian@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/7315
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- imx8m/evk_8mm/BoardConfig.mk
+++ imx8m/evk_8mm/BoardConfig.mk
@@ -137,8 +137,8 @@ endif
 KERNEL_NAME := Image
 BOARD_KERNEL_CMDLINE := init=/init androidboot.console=ttymxc1 androidboot.hardware=freescale cma=$(CMASIZE) androidboot.primary_display=imx-drm firmware_class.path=/vendor/firmware transparent_hugepage=never
 
-# Defaultly evk_8mm use BCM 1MW wifi module, if use QCOM 1PJ module, set androidboot.wifivendor=qca
-BOARD_KERNEL_CMDLINE += androidboot.wifivendor=bcm
+# Defaultly evk_8mm use QCA 1PJ wifi module, if use BCM 1MW module, set androidboot.wifivendor=bcm
+BOARD_KERNEL_CMDLINE += androidboot.wifivendor=qca
 
 ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
 ifeq ($(TARGET_USERIMAGES_USE_EXT4),true)
