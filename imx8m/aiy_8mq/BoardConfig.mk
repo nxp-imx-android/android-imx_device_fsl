0c609a3e906ccd0486210613b7ef9ef4894cdc72
guoyin.chen <guoyin.chen@nxp.com>
2019-04-03 22:48:26 +0800

    MA-14510 Remove cma size config in BOARD_KERNEL_CMDLINE
    cma size has been set in uboot based on board id. No need to set it in bootargs.
    
    Change-Id: I92a9eb02a52ba15223bbcac54f5ea7824b59420d
    Signed-off-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/8057
    Reviewed-by: ji.luo <ji.luo@nxp.com>

--- imx8m/aiy_8mq/BoardConfig.mk
+++ imx8m/aiy_8mq/BoardConfig.mk
@@ -101,7 +101,7 @@ TARGET_USES_MKE2FS := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 
 KERNEL_NAME := Image
-BOARD_KERNEL_CMDLINE := init=/init androidboot.gui_resolution=1080p androidboot.console=ttymxc0 androidboot.hardware=freescale cma=1280M androidboot.fbTileSupport=enable androidboot.primary_display=imx-drm firmware_class.path=/vendor/firmware transparent_hugepage=never
+BOARD_KERNEL_CMDLINE := init=/init androidboot.gui_resolution=1080p androidboot.console=ttymxc0 androidboot.hardware=freescale androidboot.fbTileSupport=enable androidboot.primary_display=imx-drm firmware_class.path=/vendor/firmware transparent_hugepage=never
 
 # Default wificountrycode
 BOARD_KERNEL_CMDLINE += androidboot.wificountrycode=CN
