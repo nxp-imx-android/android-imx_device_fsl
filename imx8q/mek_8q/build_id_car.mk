6982dd1d72b437241c83ae95bbfd05cf76d927a2
Jindong <jindong.yue@nxp.com>
2019-04-04 11:00:40 +0800

    MA-13372-8 [Android] update build id
    Update build id for below release:
    p9.0.0_2.0.0-ga-20190408
    p9.0.0_2.1.0-auto-ga-20190408
    
    Change-Id: If94e4bda117d85cdc41d8229a56158ab60b4e59d
    Signed-off-by: Jindong <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/8067
    Reviewed-by: juan.hao <juan.hao@nxp.com>

--- imx8q/mek_8q/build_id_car.mk
+++ imx8q/mek_8q/build_id_car.mk
@@ -18,5 +18,5 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-export BUILD_ID=2.1.0-auto-ga-rc3
+export BUILD_ID=2.1.0-auto-ga-rc4
 export BUILD_NUMBER=20190422
