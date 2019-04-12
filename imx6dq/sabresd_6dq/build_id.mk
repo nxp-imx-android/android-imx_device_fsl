eccfbe9c8dc8f7f714454fdeef2acb3ca775684e
Jindong <jindong.yue@nxp.com>
2019-04-12 10:13:38 +0800

    MA-13372-9 [Android] update build id
    Update build id for below release:
    p9.0.0_2.2.0-ga-20190415
    
    Change-Id: Ia553a4897923ed7057bc277b1b390dfe9723893f
    Signed-off-by: Jindong <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/8152
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- imx6dq/sabresd_6dq/build_id.mk
+++ imx6dq/sabresd_6dq/build_id.mk
@@ -18,5 +18,5 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-export BUILD_ID=1.0.1-ga-rc3
-export BUILD_NUMBER=20190426
+export BUILD_ID=2.2.0-ga-rc1
+export BUILD_NUMBER=20190610
