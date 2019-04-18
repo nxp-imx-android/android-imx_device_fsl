6d6ef35518e194def8b46c72a42cc6da708bf92f
guoyin.chen <guoyin.chen@nxp.com>
2019-04-18 19:19:36 +0800

    MA-14029-2 Enable neural network HAL based openvx
    Add openCL SDK header file which opencl application
    compiling need those header file
    
    Change-Id: Id731adc1b7f08c8e8f7cb42c0a37a74f022b7c42
    Signed-off-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/8211
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/9335

--- imx8m/evk_8mq/evk_8mq.mk
+++ imx8m/evk_8mq/evk_8mq.mk
@@ -102,6 +102,9 @@ PRODUCT_AAPT_CONFIG += xlarge large tvdpi hdpi xhdpi
 PRODUCT_COPY_FILES += \
     $(IMX_PATH)/imx/opencl-2d/cl_g2d.cl:$(TARGET_COPY_OUT_VENDOR)/etc/cl_g2d.cl
 
+# GPU openCL SDK header file
+-include $(FSL_PROPRIETARY_PATH)/fsl-proprietary/include/CL/cl_sdk.mk
+
 # HWC2 HAL
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
