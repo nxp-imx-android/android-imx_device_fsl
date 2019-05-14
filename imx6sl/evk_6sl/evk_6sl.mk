1b4919db5657879539e291eea93f8b3402b8e6a9
Jindong <jindong.yue@nxp.com>
2019-05-14 10:36:46 +0800

    MA-14756 remove init.{recovery}.freescale.[sd|emmc].rc
    Symlink xxx /dev/block/by-name is no need anymore.
    
    System/core/init can get boot_device from
    dts android/fstab/vendor{system}/dev node, then
    use this boot_device and related block device uevent
    to automatically symlink by-name node.
    
    See below commit in system/core:
      commit 5ee7dae840eaebdd1aec47632e2a32e1ba7dd90a
        Adds /dev/block/by-name/<partition> symlinks
    
    Change-Id: Ie43ca5f7d04be299d9576bfee76b08f8c83e7430
    Signed-off-by: Jindong <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/8434
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/9343
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- imx6sl/evk_6sl/evk_6sl.mk
+++ imx6sl/evk_6sl/evk_6sl.mk
@@ -24,8 +24,6 @@ PRODUCT_MODEL := EVK_MX6SL
 PRODUCT_COPY_FILES += \
     $(IMX_DEVICE_PATH)/app_whitelist.xml:system/etc/sysconfig/app_whitelist.xml \
     $(IMX_DEVICE_PATH)/fstab.freescale:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.freescale \
-    $(IMX_DEVICE_PATH)/init.freescale.sd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.freescale.sd.rc \
-    $(IMX_DEVICE_PATH)/init.freescale.sd.rc:root/init.recovery.freescale.sd.rc \
     $(IMX_DEVICE_PATH)/init.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.freescale.rc \
     $(IMX_DEVICE_PATH)/required_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/required_hardware.xml \
     $(IMX_DEVICE_PATH)/ueventd.freescale.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc \
