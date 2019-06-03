c6f4776f64b5ebaa3803bd3b461a3f84549a16d7
yang.tian <yang.tian@nxp.com>
2019-06-03 14:33:35 +0800

    MA-14919 Enable Bluetooth with BCM 1MW module for 8mn
    Enable Bluetooth with BCM 1MW module for 8mn.
    Remove those configs for UNITE mode.
    
    Change-Id: Ia335b8302fee8648c68f09bdc8db2437e5e6050d
    Signed-off-by: yang.tian <yang.tian@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/8605
    Reviewed-by: Wang Haoran <elven.wang@nxp.com>

--- imx8m/evk_8mn/evk_8mn.mk
+++ imx8m/evk_8mn/evk_8mn.mk
@@ -166,11 +166,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service
 
-# Qcom 1PJ Bluetooth Firmware
-PRODUCT_COPY_FILES += \
-    vendor/nxp/qca-wifi-bt/1PJ_QCA9377-3_LEA_2.0/lib/firmware/qca/tfbtnv11.bin:vendor/firmware/nvm_tlv_3.2.bin \
-    vendor/nxp/qca-wifi-bt/1PJ_QCA9377-3_LEA_2.0/lib/firmware/qca/tfbtfw11.tlv:vendor/firmware/rampatch_tlv_3.2.tlv \
-
 # BCM Bluetooth vendor config
 PRODUCT_PACKAGES += \
     bt_vendor.conf
@@ -184,14 +179,6 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     wifilogd \
     wificond
-
-# BCM bluetooth for UNITE mode
-PRODUCT_PACKAGES += \
-    libbt-vendor-unite-bcm
-
-# QCA bluetooth for UNITE mode
-PRODUCT_PACKAGES += \
-    libbt-vendor-unite-qca
 
 # BCM 1MW Wifi Firmware
 PRODUCT_COPY_FILES += \
