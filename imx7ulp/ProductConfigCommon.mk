1abb513c486acc43c605f61b7ae1f38939f7f28f
xiahong.bao <xiahong.bao@nxp.com>
2019-06-03 16:11:58 +0800

    MA-14920 Fix GTS DecodePerformanceTest
    Update ProductConfigCommon.mk for building
    media_codecs_google_video.xml based on platform
    to limit codec max resolution.
    
    Change-Id: I7e5f27dd8c5f33a937a17cbcb87beea9afe0ea0f
    Signed-off-by: xiahong.bao <xiahong.bao@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/8606
    Reviewed-by: mao xu <mao.xu@nxp.com>

--- imx7ulp/ProductConfigCommon.mk
+++ imx7ulp/ProductConfigCommon.mk
@@ -268,7 +268,8 @@ PRODUCT_PACKAGES += \
     media_codecs_ra.xml \
     media_codecs_rv.xml \
     media_codecs_performance.xml \
-    media_profiles_V1_0.xml
+    media_profiles_V1_0.xml \
+    media_codecs_google_video.xml
 
 # Omx excluded libs
 PRODUCT_PACKAGES += \
@@ -294,7 +295,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(FSL_PROPRIETARY_PATH)/fsl-proprietary/media-profile/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     $(FSL_PROPRIETARY_PATH)/fsl-proprietary/media-profile/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
-    $(FSL_PROPRIETARY_PATH)/fsl-proprietary/media-profile/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     device/fsl/common/input/Dell_Dell_USB_Entry_Keyboard.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Dell_Dell_USB_Entry_Keyboard.idc \
     device/fsl/common/input/Dell_Dell_USB_Keyboard.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Dell_Dell_USB_Keyboard.idc \
     device/fsl/common/input/Dell_Dell_USB_Keyboard.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Dell_Dell_USB_Keyboard.kl \
