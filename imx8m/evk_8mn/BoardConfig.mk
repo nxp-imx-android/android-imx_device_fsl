48e921ff82254ec790854dd5eb79d39fffe11768
Jessie Hao <juan.hao@nxp.com>
2019-06-04 13:02:37 +0800

    MA-14924 Enable MIPI panel display for 8mn.
    Add MIPI panel display for 8mn, use fsl-imx8mn-ddr4-evk-rm67191.dtb.
    
    Change-Id: Ic7ec3a2968e47901c3e3d62eaab4fb5346fff098
    Signed-off-by: Jessie Hao <juan.hao@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/8611
    Reviewed-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>

--- imx8m/evk_8mn/BoardConfig.mk
+++ imx8m/evk_8mn/BoardConfig.mk
@@ -136,6 +136,8 @@ TARGET_BOARD_DTS_CONFIG ?= imx8mn:fsl-imx8mn-trusty-evk.dtb
 else
 # imx8mn with MIPI-HDMI display and BCM wifi
 TARGET_BOARD_DTS_CONFIG ?= imx8mn:fsl-imx8mn-ddr4-evk.dtb
+# imx8mn with MIPI panel display and BCM wifi
+TARGET_BOARD_DTS_CONFIG += imx8mn-mipi-panel:fsl-imx8mn-ddr4-evk-rm67191.dtb
 # u-boot target for imx8mn_evk with LPDDR4 on board
 ifeq ($(LOW_MEMORY),true)
 TARGET_BOOTLOADER_CONFIG := imx8mn:imx8mn_evk_1g_ddr_android_defconfig
