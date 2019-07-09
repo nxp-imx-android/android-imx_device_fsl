2f12b22616ca40bb7b401ab70d48a7bc6f547272
yang.tian <yang.tian@nxp.com>
2019-07-09 19:45:53 +0800

    MA-13970 Enable BCM wifi module for evk_8mq
    Remove those multi-vendor related configs for evk_8mq
    since 8mq is using BCM 1CX module
    
    Change-Id: Ia868e86f353a4e43a23b5405170e3057c5f90956
    Signed-off-by: yang.tian <yang.tian@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/9229
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/9326

--- imx8m/evk_8mq/BoardConfig.mk
+++ imx8m/evk_8mq/BoardConfig.mk
@@ -57,27 +57,16 @@ TARGET_BOOTLOADER_POSTFIX := bin
 USE_OPENGL_RENDERER := true
 TARGET_CPU_SMP := true
 
-BOARD_WLAN_DEVICE_UNITE      := UNITE
+BOARD_WLAN_DEVICE            := bcmdhd
 WPA_SUPPLICANT_VERSION       := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER  := NL80211
 BOARD_HOSTAPD_DRIVER         := NL80211
-
-# In UNITE mode,Use default macro for bcmdhd and use unite macro for qcom
-ifeq ($(BOARD_WLAN_DEVICE_UNITE), UNITE)
-BOARD_WLAN_DEVICE            := bcmdhd
-BOARD_HOSTAPD_PRIVATE_LIB_QCA           := lib_driver_cmd_qcwcn
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB_QCA    := lib_driver_cmd_qcwcn
-BOARD_HOSTAPD_PRIVATE_LIB_BCM           := lib_driver_cmd_bcmdhd
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB_BCM    := lib_driver_cmd_bcmdhd
-endif
+BOARD_HOSTAPD_PRIVATE_LIB           := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB    := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 
 WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/brcmfmac/parameters/alternative_fw_path"
 
-WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
-
-# QCA qcacld wifi driver module
-BOARD_VENDOR_KERNEL_MODULES += \
-    $(KERNEL_OUT)/drivers/net/wireless/qcacld-2.0/wlan.ko
+#WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 
 # BCM fmac wifi driver module
 BOARD_VENDOR_KERNEL_MODULES += \
@@ -88,15 +77,6 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(IMX_DEVICE_PATH)/bluetooth
 
 # BCM 1CX BT
 BOARD_HAVE_BLUETOOTH_BCM := true
-
-# Qcom 1CQ(QCA6174) BT
-BOARD_HAVE_BLUETOOTH_QCOM := true
-BOARD_HAS_QCA_BT_ROME := true
-BOARD_HAVE_BLUETOOTH_BLUEZ := false
-QCOM_BT_USE_SIBS := true
-ifeq ($(QCOM_BT_USE_SIBS), true)
-    WCNSS_FILTER_USES_SIBS := true
-endif
 
 BOARD_USE_SENSOR_FUSION := true
 
@@ -137,9 +117,6 @@ BOARD_KERNEL_CMDLINE := init=/init androidboot.gui_resolution=1080p androidboot.
 
 # Default wificountrycode
 BOARD_KERNEL_CMDLINE += androidboot.wificountrycode=CN
-
-# Defaultly evk_8mq use BCM 1CX BCM4356 wifi module, if use QCOM qca9377 module, set androidboot.wifivendor=qca
-BOARD_KERNEL_CMDLINE += androidboot.wifivendor=bcm
 
 ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
 ifeq ($(TARGET_USERIMAGES_USE_EXT4),true)
