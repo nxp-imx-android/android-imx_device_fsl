675344829fdda43291e6a1b41f2a5beb244111e1
Jindong <jindong.yue@nxp.com>
2019-07-18 14:47:44 +0800

    MA-14403-6 Bring imx soc on q-preview (recovery)
    Fix below build error:
      device/fsl/common/recovery/recovery_ui.cpp:24:10: fatal error: 'device.h' file not found
      device/fsl/common/recovery/recovery_ui.cpp:23:10: fatal error: 'common.h' file not found
      device/fsl/common/recovery/recovery_ui.cpp:85:24: error: virtual function 'GetMenuItems' has a different return type ('const char *const *')
    
    Change-Id: I67dd95de53db02471e0a3edc0d951679ce01862c
    Signed-off-by: Jindong <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/9371
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>

--- common/recovery/Android.mk
+++ common/recovery/Android.mk
@@ -1,8 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := eng
-LOCAL_C_INCLUDES += bootable/recovery
+LOCAL_C_INCLUDES += \
+    bootable/recovery \
+    bootable/recovery/recovery_ui/include
 LOCAL_SRC_FILES := recovery_ui.cpp
 
 # should match TARGET_RECOVERY_UI_LIB set in BoardConfigCommon.mk
