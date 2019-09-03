813b1c481dfc243a04a74ff0223bd90214a92e46
Ji Luo <ji.luo@nxp.com>
2019-09-03 19:10:02 +0800

    MA-15395 Guard trusty config with PRODUCT_IMX_TRUSTY
    Sometimes we don't want to enable trusty, guard trusty
    configs with "PRODUCT_IMX_TRUSTY" so we can disable it.
    
    Test: Build and boot.
    
    Change-Id: Ic06227eab842cf938e9798662b987a09188487bb
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/9860
    Reviewed-by: Wang Haoran <elven.wang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/10004

--- imx8m/evk_8mn/BoardConfig.mk
+++ imx8m/evk_8mn/BoardConfig.mk
@@ -150,9 +150,11 @@ BOARD_PREBUILT_DTBOIMAGE := out/target/product/evk_8mn/dtbo-imx8mn.img
 # u-boot target for stand config and Trusty OS config
 TARGET_BOOTLOADER_CONFIG := imx8mn:imx8mn_ddr4_evk_android_defconfig
 TARGET_BOOTLOADER_CONFIG += imx8mn-dual:imx8mn_ddr4_evk_android_dual_defconfig
+ifeq ($(PRODUCT_IMX_TRUSTY),true)
 TARGET_BOOTLOADER_CONFIG += imx8mn-trusty:imx8mn_ddr4_evk_android_trusty_defconfig
 TARGET_BOOTLOADER_CONFIG += imx8mn-trusty-secure-unlock:imx8mn_ddr4_evk_android_trusty_secure_unlock_defconfig
 TARGET_BOOTLOADER_CONFIG += imx8mn-trusty-dual:imx8mn_ddr4_evk_android_trusty_dual_defconfig
+endif
 # u-boot target used by uuu for imx8mn_evk
 TARGET_BOOTLOADER_CONFIG += imx8mn-evk-uuu:imx8mn_ddr4_evk_android_uuu_defconfig
 # imx8mn with MIPI-HDMI display, BCM wifi and support trusty
