94db8387c8a4a8d65d21b6b264b824333d4b852c
Haoran.Wang <elven.wang@nxp.com>
2019-09-15 12:47:01 +0800

    MA-15433 Enable Widevine L3 for Android 10
    Enabled Widevine L3 for Android 10 for below builds:
      * evk_8mm
      * evk_8mq
      * mek_8q
      * mek_8q_car
      * mek_8q_car2
    
    TEST: Run exoplayer secure SD&HD stream.
    
    Change-Id: I5ec33ff442ba336bce54148714c6b30d947c5479
    Signed-off-by: Haoran.Wang <elven.wang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/10048
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-by: ji.luo <ji.luo@nxp.com>

--- imx8m/evk_8mm/evk_8mm.mk
+++ imx8m/evk_8mm/evk_8mm.mk
@@ -276,7 +276,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.1-service.widevine \
-    android.hardware.drm@1.1-service.clearkey \
+    android.hardware.drm@1.2-service.clearkey \
     libwvdrmcryptoplugin \
     libwvhidl \
     libwvdrmengine \
