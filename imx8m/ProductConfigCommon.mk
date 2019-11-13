bdac2a07219bd51db6241969892e4b9b2f13e80e
xiahong.bao <xiahong.bao@nxp.com>
2019-11-13 12:58:11 +0800

    MA-15850-6 Change imx c2 library name
    1. fslextractor -> imxextractor
    2. lib_imx_c2_videodec_core ->lib_imx_c2_vpuwrapper_dec
    3. lib_imx_c2_videoenc_core ->lib_imx_c2_vpuwrapper_enc
    
    Change-Id: Ie83aac6ffe77b31c857e4f4e2ced4e6cb526e64c
    Signed-off-by: xiahong.bao <xiahong.bao@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/10552
    Reviewed-by: mao xu <mao.xu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/10900

--- imx8m/ProductConfigCommon.mk
+++ imx8m/ProductConfigCommon.mk
@@ -260,7 +260,7 @@ PRODUCT_PACKAGES += \
     libavcodec \
     libavutil \
     libfsl_jpeg_enc_arm11_elinux \
-    libfslextractor \
+    libimxextractor \
     libfslxec \
     libstagefrighthw \
     libswresample \
