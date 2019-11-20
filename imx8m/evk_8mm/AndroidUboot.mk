8051e5550027eb849b827f0ec415abc82f306ee7
Ji Luo <ji.luo@nxp.com>
2019-11-20 09:42:58 +0800

    MA-15817-1 Update prebuilt firmwares for imx8m
    Use new hdmi and ddr training firmwares in linux-firmware-imx.
    
    Test: boot and mppubk generate.
    
    Change-Id: Iefc3cb766063f46b0c523bff39469fc9bcce573e
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/10637
    Reviewed-by: zhang bo <bo.zhang@nxp.com>
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- imx8m/evk_8mm/AndroidUboot.mk
+++ imx8m/evk_8mm/AndroidUboot.mk
@@ -8,13 +8,12 @@ define build_imx_uboot
 	cp $(UBOOT_OUT)/u-boot-nodtb.$(strip $(1)) $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/.; \
 	cp $(UBOOT_OUT)/spl/u-boot-spl.bin  $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/.; \
 	cp $(UBOOT_OUT)/tools/mkimage  $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/mkimage_uboot; \
-	cp $(FSL_PROPRIETARY_PATH)/fsl-proprietary/uboot-firmware/imx8m/signed_hdmi_imx8m.bin  $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/.; \
 	if [ `echo $(2) | cut -d '-' -f2` == "ddr4" ]; then \
 		cp $(UBOOT_OUT)/arch/arm/dts/fsl-imx8mm-ddr4-evk.dtb $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/.; \
-		cp $(FSL_PROPRIETARY_PATH)/fsl-proprietary/uboot-firmware/imx8m/ddr4_* $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/.; \
+		cp $(FSL_PROPRIETARY_PATH)/linux-firmware-imx/firmware/ddr/synopsys/ddr4* $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/.; \
 	else \
 		cp $(UBOOT_OUT)/arch/arm/dts/fsl-imx8mm-evk.dtb  $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/.; \
-		cp $(FSL_PROPRIETARY_PATH)/fsl-proprietary/uboot-firmware/imx8m/lpddr4_pmu_train_* $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/.; \
+		cp $(FSL_PROPRIETARY_PATH)/linux-firmware-imx/firmware/ddr/synopsys/lpddr4_pmu_train* $(IMX_MKIMAGE_PATH)/imx-mkimage/iMX8M/.; \
 	fi; \
 	$(MAKE) -C $(IMX_PATH)/arm-trusted-firmware/ PLAT=`echo $(2) | cut -d '-' -f1` clean; \
 	if [ `echo $(2) | cut -d '-' -f2` == "trusty" ] && [ `echo $(2) | rev | cut -d '-' -f1` != "uuu" ]; then \
