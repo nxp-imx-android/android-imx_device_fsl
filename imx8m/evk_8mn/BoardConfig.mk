98d7e8ce652bc55a0705b4482a5ff3a6d346b323
zhang sanshan <pete.zhang@nxp.com>
2019-12-04 18:33:56 +0800

    MA-16031 [Android] there is no need use openCL to do NV12->NV21 in cameraserver
    remove OPENCL_2D_IN_CAMERA which is not used now in cameraserver
    
    Change-Id: Ic6588d6ed9c3dd1b381a92e1a1a837284c1938c5
    Signed-off-by: zhang sanshan <pete.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/10752
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- imx8m/evk_8mn/BoardConfig.mk
+++ imx8m/evk_8mn/BoardConfig.mk
@@ -75,11 +75,6 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 
 BOARD_HAVE_USB_CAMERA := true
 
-# whether to accelerate camera service with openCL
-# it will make camera service load the opencl lib in vendor
-# and break the full treble rule
-#OPENCL_2D_IN_CAMERA := true
-
 USE_ION_ALLOCATOR := true
 USE_GPU_ALLOCATOR := false
 
