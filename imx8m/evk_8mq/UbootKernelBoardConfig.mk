195e18b447e4bf4cb889bc116848ac17f58adb19
faqiang.zhu <faqiang.zhu@nxp.com>
2019-12-10 23:04:20 +0800

    MA-16072 fix error in setting rbindex in FIT&Container
    now uboot is build independently, "BOOTLOADER_RBINDEX" should be moved
    to "UbootKernelBoardConfig.mk" so it can be accessed in "uboot.mk"
    
    make "$(UBOOTENVSH)" as a phony target, so it can be updated every time
    when build images. the ubootenv.sh is moved to "$(UBOOT_OUT)" directory
    from "/tmp" to avoid potential affect on others use the same server to
    build images.
    
    default "/bin/sh" used by "make" does not support "source" command,
    replace it with "."
    
    after the rollback index properly set, dual bootloader with trusty on
    evk_8mm can now boot up.
    
    Change-Id: Iebccdf1657d5297298703e58f68c074edc5c925a
    Signed-off-by: faqiang.zhu <faqiang.zhu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/10786
    Reviewed-by: ji.luo <ji.luo@nxp.com>

--- imx8m/evk_8mq/UbootKernelBoardConfig.mk
+++ imx8m/evk_8mq/UbootKernelBoardConfig.mk
@@ -20,3 +20,6 @@ TARGET_KERNEL_DEFCONFIG := android_defconfig
 # absolute path is used, not the same as relative path used in AOSP make
 TARGET_DEVICE_DIR := $(patsubst %/, %, $(dir $(realpath $(lastword $(MAKEFILE_LIST)))))
 
+# define bootloader rollback index
+BOOTLOADER_RBINDEX ?= 0
+
