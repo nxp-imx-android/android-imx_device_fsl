5c8360c1368ae63263a0750731ea26cee0530897
Ji Luo <ji.luo@nxp.com>
2019-12-12 13:49:14 +0800

    MA-16078-2 Support non product partition build
    Add variable 'IMX_NO_PRODUCT_PARTITION' to support non
    product partition build. This commit will remove the
    product partition in GPT and enlarge the system partition
    to 2816MB as it may get larger in the further.
    
    This config is to align the image overlay in Android9, but
    please note the partition size of system and vendor are
    different. This config will be disabled by default.
    
    Test: build and boot on imx8qxp and imx8mm.
    
    Change-Id: Iac02a5b8a54de54d0b734ed1b2bdf783430daf7c
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/10799
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- imx8m/evk_8mq/SharedBoardConfig.mk
+++ imx8m/evk_8mq/SharedBoardConfig.mk
@@ -4,6 +4,9 @@ TARGET_KERNEL_ARCH := arm64
 #Enable this to include trusty support
 PRODUCT_IMX_TRUSTY := true
 
+#Enable this to disable product partition build.
+#IMX_NO_PRODUCT_PARTITION := true
+
 # BCM fmac wifi driver module
 BOARD_VENDOR_KERNEL_MODULES += \
     $(KERNEL_OUT)/drivers/net/wireless/broadcom/brcm80211/brcmfmac/brcmfmac.ko \
