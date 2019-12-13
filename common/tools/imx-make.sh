39f5304c53cde60fb8af39f066ac893c568c817e
faqiang.zhu <faqiang.zhu@nxp.com>
2019-12-13 16:21:47 +0800

    MA-15722-3 add frequently used targets in imx-make.sh
    add below targets support in imx-make.sh, now these images can be easily
    built with one command, this would be convenient to build for debug
    purpose.
    
    	bootimage
    	dtboimage
    	vendorimage
    
    Change-Id: I7545a601b13a80c8f738138f8b710bfcc369c6fb
    Signed-off-by: faqiang.zhu <faqiang.zhu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/10823
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>

--- common/tools/imx-make.sh
+++ common/tools/imx-make.sh
@@ -11,8 +11,11 @@ cat << EOF
         options:
            -h/--help               display this help info
            -j[<num>]               specify the number of parallel jobs when build the target, the number after -j should be greater than 0
-           bootloader              bootloader will be compiled, Android rootfs will not be compiled
-           kernel                  kernel, include related dts will be compiled, Android rootfs will not be compiled
+           bootloader              bootloader will be compiled
+           kernel                  kernel, include related dts will be compiled
+           dtboimage               dtbo images will be built out
+           bootimage               boot.img will be built out
+           vendorimage             vendor.img will be built out
            -c                      use clean build for kernel, not incremental build
 
 
@@ -56,10 +59,13 @@ if [ -z ${OUT} ] || [ -z ${TARGET_PRODUCT} ]; then
 fi
 
 # global variables
-build_bootloader_flag=0
+build_bootloader_kernel_flag=0
+build_android_flag=0
 build_bootloader=""
-build_kernel_flag=0
 build_kernel=""
+build_bootimage=""
+build_dtboimage=""
+build_vendorimage=""
 parallel_option=""
 clean_build=0
 
@@ -70,16 +76,31 @@ for arg in ${args[*]} ; do
         -h) help;;
         --help) help;;
         -c) clean_build=1;;
-        bootloader) build_bootloader_flag=1; build_bootloader="bootloader";;
-        kernel) build_kernel_flag=1; build_kernel="${OUT}/kernel";;
+        bootloader) build_bootloader_kernel_flag=1;
+                    build_bootloader="bootloader";;
+        kernel) build_bootloader_kernel_flag=1;
+                    build_kernel="${OUT}/kernel";;
+        bootimage) build_bootloader_kernel_flag=1;
+                    build_android_flag=1;
+                    build_kernel="${OUT}/kernel";
+                    build_bootimage="bootimage";;
+        dtboimage) build_kernel_flag=1;
+                    build_android_flag=1;
+                    build_kernel="${OUT}/kernel";
+                    build_dtboimage="dtboimage";;
+        vendorimage) build_bootloader_kernel_flag=1;
+                    build_android_flag=1;
+                    build_kernel="${OUT}/kernel";
+                    build_vendorimage="vendorimage";;
         *) handle_special_arg ${arg};;
     esac
 done
 
 # if bootloader and kernel not in arguments, all need to be made
-if [ ${build_bootloader_flag} -eq 0 ] && [ ${build_kernel_flag} -eq 0 ]; then
+if [ ${build_bootloader_kernel_flag} -eq 0 ] && [ ${build_android_flag} -eq 0 ]; then
     build_bootloader="bootloader";
     build_kernel="${OUT}/kernel";
+    build_android_flag=1
 fi
 
 product_makefile=`pwd`/`find device/fsl -maxdepth 4 -name "${TARGET_PRODUCT}.mk"`;
@@ -97,11 +118,11 @@ soc_path=${soc_path} product_path=${product_path} fsl_git_path=${fsl_git_path} c
     make -C ./ -f ${fsl_git_path}/common/build/Makefile ${parallel_option} \
     ${build_bootloader} ${build_kernel} </dev/null || exit
 
-if [ ${build_bootloader_flag} -eq 0 ] && [ ${build_kernel_flag} -eq 0 ]; then
+if [ ${build_android_flag} -eq 1 ]; then
     # source envsetup.sh before building Android rootfs, the time spent on building uboot/kernel
     # before this does not count in the final result
     source build/envsetup.sh
-    make ${parallel_option};
+    make ${parallel_option} ${build_bootimage} ${build_dtboimage} ${build_vendorimage}
 fi
 
 # copy the uboot output to ${OUT_DIR}
