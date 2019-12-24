0f84f11f2e51bc935a7db5e5a18955ac23227e2b
Ji Luo <ji.luo@nxp.com>
2019-12-24 20:49:28 +0800

    MA-16198-3 Add build target for imx8mp
    Add lunch combo for imx8mp target, this is based on imx8mn.
    
    Test: builds.
    
    Change-Id: If1853145617826eec862a238871ba25e192af9a9
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/11035
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>

---
+++ imx8m/evk_8mp/UbootKernelBoardConfig.mk
@@ -0,0 +1,19 @@
+# from BoardConfig.mk
+TARGET_BOOTLOADER_POSTFIX := bin
+UBOOT_POST_PROCESS := true
+
+# u-boot target
+TARGET_BOOTLOADER_CONFIG := imx8mp:imx8mp_evk_android_defconfig
+TARGET_BOOTLOADER_CONFIG += imx8mp-evk-uuu:imx8mp_evk_android_uuu_defconfig
+
+TARGET_KERNEL_DEFCONFIG := imx_v8_android_defconfig
+
+TARGET_KERNEL_ADDITION_DEFCONF := android_addition_defconfig
+
+
+# absolute path is used, not the same as relative path used in AOSP make
+TARGET_DEVICE_DIR := $(patsubst %/, %, $(dir $(realpath $(lastword $(MAKEFILE_LIST)))))
+
+# define bootloader rollback index
+BOOTLOADER_RBINDEX ?= 0
+
