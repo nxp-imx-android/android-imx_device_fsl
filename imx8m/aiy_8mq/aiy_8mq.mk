d81681557e35d0991ba9bfad67a99198a97df0a0
Richard Liu <xuegang.liu@nxp.com>
2019-12-25 12:18:43 +0000

    MA-16168 Add back gralloc_viv on 5.4 kernel build
    Boot issue with gralloc_viv on 5.4 kernel has been fixed,
    revert previous patch.
    
    Revert "MA-16096-1 remove gralloc_viv"
    This reverts commit 68763bfdae731879580e5003f71160c903f371df.
    
    Change-Id: I89f2adc2412044c25f7a4ded0d9087cedf165d45
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/10966
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>

--- imx8m/aiy_8mq/aiy_8mq.mk
+++ imx8m/aiy_8mq/aiy_8mq.mk
@@ -121,6 +121,7 @@ PRODUCT_PACKAGES += \
         libEGL_VIVANTE \
         libGLESv1_CM_VIVANTE \
         libGLESv2_VIVANTE \
+        gralloc_viv.imx8 \
         libGAL \
         libGLSLC \
         libVSC \
