5694617baafd4cc74b6085772611715eb1190b57
Ji Luo <ji.luo@nxp.com>
2020-02-18 19:30:51 +0800

    MA-16419-3 Enable trusty for imx8mp
    Add bootloader build target to enable imx8mp trusty
    
    Test: boots with trusty.
    
    Change-Id: I4634a5059f04388d1c5727942d1743ccd95e1eb6
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/11362
    Reviewed-by: Wang Haoran <elven.wang@nxp.com>

--- imx8m/evk_8mp/UbootKernelBoardConfig.mk
+++ imx8m/evk_8mp/UbootKernelBoardConfig.mk
@@ -4,6 +4,8 @@ UBOOT_POST_PROCESS := true
 
 # u-boot target
 TARGET_BOOTLOADER_CONFIG := imx8mp:imx8mp_evk_android_defconfig
+TARGET_BOOTLOADER_CONFIG += imx8mp-trusty:imx8mp_evk_android_trusty_defconfig
+TARGET_BOOTLOADER_CONFIG += imx8mp-trusty-secure-unlock:imx8mp_evk_android_trusty_secure_unlock_defconfig
 TARGET_BOOTLOADER_CONFIG += imx8mp-evk-uuu:imx8mp_evk_android_uuu_defconfig
 
 TARGET_KERNEL_DEFCONFIG := imx_v8_android_defconfig
