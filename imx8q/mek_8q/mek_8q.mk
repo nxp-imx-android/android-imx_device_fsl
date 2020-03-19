20e488eb498a072bed3537d1458272a4d83a57b9
faqiang.zhu <faqiang.zhu@nxp.com>
2020-03-19 16:57:45 +0800

    MA-16457-10 do not pass the partition size for lpmake
    simglen was used to get the raw image size of system.img, vendor.img and
    product.img, this size will be set to the partition size in lpmake
    command.
    
    lpmake now can itself detect the size of the partition if a image is
    specified for the partition, lpmake will use similar logic as in simglen
    to get the size.
    
    Also, simglen will not be copied to ${OUT} directory.
    
    Change-Id: I433ed2a545b78829ecd3a754cc5d8368a066d27e
    Signed-off-by: faqiang.zhu <faqiang.zhu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/11729
    Reviewed-by: Wang Haoran <elven.wang@nxp.com>

--- imx8q/mek_8q/mek_8q.mk
+++ imx8q/mek_8q/mek_8q.mk
@@ -90,9 +90,7 @@ PRODUCT_COPY_FILES += \
 
 ifeq ($(TARGET_USE_DYNAMIC_PARTITIONS),true)
 PRODUCT_COPY_FILES += \
-    $(FSL_PROPRIETARY_PATH)/fsl-proprietary/dynamic_partiton_tools/simglen:simglen \
     $(FSL_PROPRIETARY_PATH)/fsl-proprietary/dynamic_partiton_tools/lpmake:lpmake \
-    $(FSL_PROPRIETARY_PATH)/fsl-proprietary/dynamic_partiton_tools/simglen.exe:simglen.exe \
     $(FSL_PROPRIETARY_PATH)/fsl-proprietary/dynamic_partiton_tools/lpmake.exe:lpmake.exe
 endif
 
