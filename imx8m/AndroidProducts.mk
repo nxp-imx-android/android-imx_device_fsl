e2127a2a85e5bb15beebd3efd4e02503731d72bc
Haoran.Wang <elven.wang@nxp.com>
2020-05-12 12:04:48 +0800

    MA-17103 Remove all aiy_8mq related codes
    Change-Id: Ib9a51724b3b143ad7de153da5aaf671567a9868a
    Signed-off-by: Haoran.Wang <elven.wang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/12146
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- imx8m/AndroidProducts.mk
+++ imx8m/AndroidProducts.mk
@@ -32,7 +32,6 @@ PRODUCT_MAKEFILES := \
   $(LOCAL_DIR)/evk_8mm/evk_8mm.mk \
   $(LOCAL_DIR)/evk_8mm/evk_8mm_ddr4.mk \
   $(LOCAL_DIR)/evk_8mm/evk_8mm_drm.mk \
-  $(LOCAL_DIR)/aiy_8mq/aiy_8mq.mk \
   $(LOCAL_DIR)/evk_8mn/evk_8mn.mk \
   $(LOCAL_DIR)/evk_8mp/evk_8mp.mk
 
@@ -47,8 +46,6 @@ COMMON_LUNCH_CHOICES := \
   evk_8mm_ddr4-userdebug \
   evk_8mm_drm-user \
   evk_8mm_drm-userdebug \
-  aiy_8mq-user \
-  aiy_8mq-userdebug \
   evk_8mn-user \
   evk_8mn-userdebug \
   evk_8mp-user \
