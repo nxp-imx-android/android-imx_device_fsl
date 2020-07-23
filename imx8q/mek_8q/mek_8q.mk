f1f6274fba35fd30203f8d73a7b4dac74e34f4ac
zhai.he <zhai.he@nxp.com>
2020-07-23 18:02:07 +0800

    MA-17547 Upgrade Widevine L3 to 1.3
    1.Delete android.hardware.drm@1.2-service.widevine、
      android.hardware.drm@1.2-service.clearkey
      and add android.hardware.drm@1.3-service.widevine、
      android.hardware.drm@1.3-service.clearkey from PRODUCT_PACKAGES
      in the makefile of evk_8mm, evk_8mp, mek 8q.
    2.Add the android.hardware.drm@1.3-service.widevine security context.
    
    Change-Id: I24681fa7cea37bbd5844d8a898f8680d070cdb5f
    Signed-off-by: zhai.he <zhai.he@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/12896
    Reviewed-by: Wang Haoran <elven.wang@nxp.com>

--- imx8q/mek_8q/mek_8q.mk
+++ imx8q/mek_8q/mek_8q.mk
@@ -457,8 +457,8 @@ endif
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.widevine \
-    android.hardware.drm@1.2-service.clearkey \
+    android.hardware.drm@1.3-service.widevine \
+    android.hardware.drm@1.3-service.clearkey \
     libwvdrmcryptoplugin \
     libwvhidl \
     libwvdrmengine \
