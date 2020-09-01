180c285e3ccfc22ae43ea333f3622e05cff2424e
Jindong <jindong.yue@nxp.com>
2020-09-01 10:52:19 +0800

    MA-17545-1 replace_kernel.sh: support boot header v2
    Boot header v2 needs more params than v3, such as
    kernel load base address, kernel/ramdisk offset
    and command line.
    
    If you meet below error:
    error while loading shared libraries:
      /usr/lib/x86_64-linux-gnu/libc++.so: file too short
    you can change libc++.so to another version, like:
      cd /usr/lib/x86_64-linux-gnu
      mv libc++.so libc++.so-short
      sudo ln -s  libc++.so.1.0 libc++.so
    
    Signed-off-by: Jindong <jindong.yue@nxp.com>
    Change-Id: If6833e1cfc214d338c472c7fb530d6f1cf66dc98
    Reviewed-on: http://androidsource.ap.freescale.net/project/13317
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>

--- common/tools/replace_kernel.sh
+++ common/tools/replace_kernel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+BOARD_KERNEL_OFFSET=0x00080000
 help()
 {
     echo "Usage: Using Image to replace old one in boot.img, then generate new_boot.img"
@@ -40,23 +41,57 @@ if [ ! -f ${boot_img} -o ! -f ${Image} ]; then
 fi
 
 unpack_out_dir="unpack_out"
+unpack_log_temp_file="unpack.log.tmp"
 unpack_log_file="unpack.log"
-./unpack_bootimg --boot_img ${boot_img} --out ${unpack_out_dir} > ${unpack_log_file}
+
+./unpack_bootimg --boot_img ${boot_img} --out ${unpack_out_dir} > ${unpack_log_temp_file}
+# remove all invisible characters
+sed $'s/[^[:print:]\t]//g' ${unpack_log_temp_file} > ${unpack_log_file}
+rm ${unpack_log_temp_file}
 
 key_os_version="os version: "
 key_os_patch_level="os patch level: "
 key_header_version="boot image header version: "
+key_cmdline="command line args: "
+key_additional_cmdline="additional command line args: "
+key_kernel_load_address="kernel load address: "
+key_ramdisk_load_address="ramdisk load address: "
 
 os_version=`grep "$key_os_version" $unpack_log_file`
 os_version=${os_version#*${key_os_version}}
 os_patch_level=`grep "$key_os_patch_level" $unpack_log_file`
-os_patch_level=${os_patch_level#*${key_os_patch_level}}
+os_patch_level=${os_patch_level#*${key_os_patch_level}}"-05"
 header_version=`grep "$key_header_version" $unpack_log_file`
 header_version=${header_version#*${key_header_version}}
+cmdline=`grep "$key_cmdline" $unpack_log_file | grep -v "${key_additional_cmdline}"`
+cmdline=${cmdline#*${key_cmdline}}
+kernel_load_address=`grep "$key_kernel_load_address" $unpack_log_file`
+kernel_load_address=${kernel_load_address#*${key_kernel_load_address}}
+ramdisk_load_address=`grep "$key_ramdisk_load_address" $unpack_log_file`
+ramdisk_load_address=${ramdisk_load_address#*${key_ramdisk_load_address}}
+kernel_offset=${BOARD_KERNEL_OFFSET}
+board_kernel_base=$((${kernel_load_address}-${kernel_offset}))
+ramdisk_offset=$((${ramdisk_load_address}-${board_kernel_base}))
+
+echo "os version: $os_version"
+echo "os patch level: $os_patch_level"
+echo "header version: $header_version"
 
 echo "Replace ${Image} in ${boot_img} ..."
-./mkbootimg --kernel ${Image} --ramdisk ${unpack_out_dir}/ramdisk --os_version ${os_version} --os_patch_level ${os_patch_level} \
+if [ ${header_version} != "3" ]; then
+    echo "cmdline: $cmdline"
+    echo "kernel load address: $kernel_load_address"
+    echo "ramdisk load address: $ramdisk_load_address"
+    echo "kernel offset: $kernel_offset"
+    echo "ramdisk offset: $ramdisk_offset"
+
+    ./mkbootimg --kernel ${Image} --ramdisk ${unpack_out_dir}/ramdisk --os_version ${os_version} --os_patch_level ${os_patch_level} \
+    --header_version ${header_version} --cmdline "${cmdline}" --base ${board_kernel_base} --kernel_offset ${kernel_offset} \
+    --ramdisk_offset ${ramdisk_offset} --output ${new_boot_img}
+else
+    ./mkbootimg --kernel ${Image} --ramdisk ${unpack_out_dir}/ramdisk --os_version ${os_version} --os_patch_level ${os_patch_level} \
     --header_version ${header_version} --output ${new_boot_img}
+fi
 
 echo "Done: ${new_boot_img}"
 
