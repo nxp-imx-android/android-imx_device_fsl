1f84ee279b441e82520906a121cc860698ebbc7a
Jessie Hao <juan.hao@nxp.com>
2020-12-08 17:35:56 +0800

    MA-18371 Remove input_methods and app_widgets from car builds.
    Below commit under frameworks/native/data/etc/car_core_hardware.xml
    remove android.software.input_methods from car builds.
    2c60d0f0f528be1ed0bbe6d6ef79acc4d40ae832
    Here align with it to pass below CTS for car builds.
    CtsWindowManagerDeviceTestCases
    android.server.wm.ActivityViewTest#testInputMethod
    android.server.wm.WindowInsetsAnimationImeTests#testZeroInsetsImeAnimates
    android.server.wm.WindowInsetsAnimationImeTests#testImeAnimationCallbacksShowAndHide
    
    Below commit frameworks/native/data/etc/car_core_hardware.xml
    remove android.software.app_widgets from car builds.
    427d44716a52734e3b2b86283200919d32adf9b8
    Here align with it.
    
    Change-Id: Ifc525fd6335111ea873074c8c74dc1523b85d0f7
    Signed-off-by: Jessie Hao <juan.hao@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/14225
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>

--- imx8q/mek_8q/mek_8q.mk
+++ imx8q/mek_8q/mek_8q.mk
@@ -187,7 +187,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
-    frameworks/native/data/etc/android.software.app_widgets.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.app_widgets.xml \
     frameworks/native/data/etc/android.software.backup.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.backup.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
@@ -207,6 +206,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.external.xml:vendor/etc/permissions/android.hardware.camera.external.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.software.app_widgets.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.app_widgets.xml \
     frameworks/native/data/etc/android.software.device_admin.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_admin.xml \
     frameworks/native/data/etc/android.software.managed_users.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.managed_users.xml \
     frameworks/native/data/etc/android.software.print.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.print.xml
