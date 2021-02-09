a7ee9d7410dbcd68f35f9eebe7b1211f531326f6
xiahong.bao <xiahong.bao@nxp.com>
2021-02-09 12:09:24 +0800

    MA-18089-3 Update kernel header files
    After merging below commit in kernel:
    arm64: imx8mx: vpu: integrate vsi 20210205 release
    Update kernel header files imx_vpu.h
    
    Signed-off-by: xiahong.bao <xiahong.bao@nxp.com>
    Change-Id: I9572d8eb6e9b8e3841557aa43a28002bf7183014
    Reviewed-on: http://androidsource.ap.freescale.net/project/15038
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>

--- common/kernel-headers/linux/imx_vpu.h
+++ common/kernel-headers/linux/imx_vpu.h
@@ -20,8 +20,16 @@
 #define _UAPI__LINUX_IMX_VPU_H
 #include <linux/videodev2.h>
 #include <linux/v4l2-controls.h>
+#define VSI_V4L2_CMPTYPE_ROI (V4L2_CTRL_COMPOUND_TYPES + 100)
+#define VSI_V4L2_CMPTYPE_IPCM (V4L2_CTRL_COMPOUND_TYPES + 101)
+#define VSI_V4L2_CMPTYPE_HDR10META (V4L2_CTRL_COMPOUND_TYPES + 102)
 #define V4L2_CID_NON_FRAME (V4L2_CID_USER_IMX_BASE)
 #define V4L2_CID_DIS_REORDER (V4L2_CID_USER_IMX_BASE + 1)
+#define V4L2_CID_ROI_COUNT (V4L2_CID_USER_IMX_BASE + 2)
+#define V4L2_CID_ROI (V4L2_CID_USER_IMX_BASE + 3)
+#define V4L2_CID_IPCM_COUNT (V4L2_CID_USER_IMX_BASE + 4)
+#define V4L2_CID_IPCM (V4L2_CID_USER_IMX_BASE + 5)
+#define V4L2_CID_HDR10META (V4L2_CID_USER_IMX_BASE + 6)
 #define V4L2_MAX_ROI_REGIONS 8
 struct v4l2_enc_roi_param {
   struct v4l2_rect rect;
@@ -35,8 +43,6 @@ struct v4l2_enc_roi_params {
   __u32 config_store;
   __u32 reserved[2];
 };
-#define V4L2_CID_ROI_COUNT (V4L2_CID_USER_IMX_BASE + 2)
-#define V4L2_CID_ROI (V4L2_CID_USER_IMX_BASE + 3)
 #define V4L2_MAX_IPCM_REGIONS 2
 struct v4l2_enc_ipcm_param {
   struct v4l2_rect rect;
@@ -49,14 +55,28 @@ struct v4l2_enc_ipcm_params {
   __u32 config_store;
   __u32 reserved[2];
 };
-#define V4L2_CID_IPCM_COUNT (V4L2_CID_USER_IMX_BASE + 4)
-#define V4L2_CID_IPCM (V4L2_CID_USER_IMX_BASE + 5)
+struct v4l2_hdr10_meta {
+  __u32 redPrimary[2];
+  __u32 greenPrimary[2];
+  __u32 bluePrimary[2];
+  __u32 whitePoint[2];
+  __u32 maxMasteringLuminance;
+  __u32 minMasteringLuminance;
+  __u32 maxContentLightLevel;
+  __u32 maxFrameAverageLightLevel;
+};
 #define V4L2_DEC_CMD_IMX_BASE (0x08000000)
 #define V4L2_DEC_CMD_RESET (V4L2_DEC_CMD_IMX_BASE + 1)
 #define V4L2_EVENT_CODEC_ERROR (V4L2_EVENT_PRIVATE_START + 1)
 #define V4L2_EVENT_SKIP (V4L2_EVENT_PRIVATE_START + 2)
 #define V4L2_EVENT_CROPCHANGE (V4L2_EVENT_PRIVATE_START + 3)
 #define V4L2_EVENT_INVALID_OPTION (V4L2_EVENT_PRIVATE_START + 4)
+enum {
+  UNKONW_WARNING = - 1,
+  RIOREGION_NOTALLOW,
+  IPCMREGION_NOTALLOW,
+  LEVEL_UPDATED,
+};
 #define V4L2_PIX_FMT_BGR565 v4l2_fourcc('B', 'G', 'R', 'P')
 #define V4L2_PIX_FMT_NV12X v4l2_fourcc('N', 'V', 'X', '2')
 #define V4L2_PIX_FMT_DTRC v4l2_fourcc('D', 'T', 'R', 'C')
