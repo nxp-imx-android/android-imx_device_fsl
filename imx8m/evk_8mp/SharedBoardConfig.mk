d78bc941a7a5048f20d263eb8c1886d68bc2ae2f
Haoran.Wang <elven.wang@nxp.com>
2021-03-30 19:21:44 +0800

    MA-18860-1 support ion_unmapped_heap for GKI
    Change-Id: Ia3efb4544d8fc840c7a92bbdc36b8f9253603b8a
    Signed-off-by: Haoran.Wang <elven.wang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/15447
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>

--- imx8m/evk_8mp/SharedBoardConfig.mk
+++ imx8m/evk_8mp/SharedBoardConfig.mk
@@ -148,6 +148,7 @@ BOARD_VENDOR_RAMDISK_KERNEL_MODULES +=     \
     $(KERNEL_OUT)/drivers/staging/android/ion/ion-alloc.ko \
     $(KERNEL_OUT)/drivers/staging/android/ion/heaps/ion_sys_heap.ko \
     $(KERNEL_OUT)/drivers/staging/android/ion/heaps/ion_cma_heap.ko \
+    $(KERNEL_OUT)/drivers/staging/android/ion/heaps/ion_unmapped_heap.ko \
     $(KERNEL_OUT)/drivers/reset/reset-imx7.ko \
     $(KERNEL_OUT)/drivers/phy/freescale/phy-fsl-imx8mp-lvds.ko \
     $(KERNEL_OUT)/drivers/phy/freescale/phy-fsl-samsung-hdmi.ko \
