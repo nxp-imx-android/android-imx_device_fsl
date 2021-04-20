d5c9270171ba1fd907d3b0bdb4617e82812925a8
Jindong Yue <jindong.yue@nxp.com>
2021-04-20 09:36:07 +0800

    MA-18932 GKI: Add lpa_ctrl module
    Add lpa_ctrl module to support switching lpa mode in ATF.
      echo 1 > /sys/lpa_ctrl/lpa
    
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Change-Id: I45e58031d351732ca5fbfe2907e8b923c48945ad
    Reviewed-on: http://androidsource.ap.freescale.net/project/15606
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>

--- imx8m/evk_8mp/SharedBoardConfig.mk
+++ imx8m/evk_8mp/SharedBoardConfig.mk
@@ -136,6 +136,7 @@ endif
 
 ifneq ($(IMX8MP_USES_GKI),)
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES +=     \
+    $(KERNEL_OUT)/drivers/soc/imx/lpa_ctrl.ko \
     $(KERNEL_OUT)/drivers/soc/imx/soc-imx8m.ko \
     $(KERNEL_OUT)/drivers/clk/imx/mxc-clk.ko \
     $(KERNEL_OUT)/drivers/clk/imx/clk-audiomix.ko \
