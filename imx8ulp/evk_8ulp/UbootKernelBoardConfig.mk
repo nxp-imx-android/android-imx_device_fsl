cf51f7ffb74208a8c60b0c063f49a554e4c15aef
Jindong Yue <jindong.yue@nxp.com>
2021-04-27 14:04:07 +0800

    MA-18983-1 Support building 8ulp bootloader
    Change-Id: Id70de1fe73b0cf50bc61eb95ce08599061d8170b
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/15941
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>

--- imx8ulp/evk_8ulp/UbootKernelBoardConfig.mk
+++ imx8ulp/evk_8ulp/UbootKernelBoardConfig.mk
@@ -1,28 +1,8 @@
 TARGET_BOOTLOADER_POSTFIX := bin
 UBOOT_POST_PROCESS := true
 
-# u-boot target for imx8mm_evk with DDR4 on board
-TARGET_BOOTLOADER_CONFIG := imx8mm-ddr4:imx8mm_ddr4_evk_android_defconfig
-# u-boot target for imx8mm_evk with LPDDR4 on board
-ifeq ($(LOW_MEMORY),true)
-  TARGET_BOOTLOADER_CONFIG += imx8mm:imx8mm_evk_1g_ddr_android_defconfig
-else
-  TARGET_BOOTLOADER_CONFIG += imx8mm:imx8mm_evk_android_defconfig
-  TARGET_BOOTLOADER_CONFIG += imx8mm-dual:imx8mm_evk_android_dual_defconfig
-endif
-TARGET_BOOTLOADER_CONFIG += imx8mm-4g:imx8mm_evk_4g_android_defconfig
-ifeq ($(PRODUCT_IMX_TRUSTY),true)
-  TARGET_BOOTLOADER_CONFIG += imx8mm-trusty:imx8mm_evk_android_trusty_defconfig
-  TARGET_BOOTLOADER_CONFIG += imx8mm-trusty-secure-unlock:imx8mm_evk_android_trusty_secure_unlock_defconfig
-  TARGET_BOOTLOADER_CONFIG += imx8mm-trusty-dual:imx8mm_evk_android_trusty_dual_defconfig
-  TARGET_BOOTLOADER_CONFIG += imx8mm-trusty-4g:imx8mm_evk_4g_android_trusty_defconfig
-endif
-
-# u-boot target used by uuu for imx8mm_evk with DDR4 on board
-TARGET_BOOTLOADER_CONFIG += imx8mm-ddr4-evk-uuu:imx8mm_ddr4_evk_android_uuu_defconfig
-# u-boot target used by uuu for imx8mm_evk with LPDDR4 on board
-TARGET_BOOTLOADER_CONFIG += imx8mm-evk-uuu:imx8mm_evk_android_uuu_defconfig
-TARGET_BOOTLOADER_CONFIG += imx8mm-4g-evk-uuu:imx8mm_evk_4g_android_uuu_defconfig
+# u-boot target for imx8ulp_evk board
+TARGET_BOOTLOADER_CONFIG := imx8ulp:imx8ulp_evk_android_defconfig
 
 # imx8mm kernel defconfig
 ifeq ($(IMX8MM_USES_GKI),true)
