e9b23ea2b02bd5a06b28a3453308bcd1225a9b07
zhang sanshan <pete.zhang@nxp.com>
2021-05-08 17:04:02 +0800

    MA-18995 correct the license
    Change-Id: I45c38b3febbf6715b22de74208af732e1af1fcd7
    Signed-off-by: zhang sanshan <pete.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/15758
    Reviewed-by: zhang bo <bo.zhang@nxp.com>

--- common/kernel-headers/linux/ion_imx.h
+++ common/kernel-headers/linux/ion_imx.h
@@ -1,17 +1,27 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-/*
- * Copyright 2021 NXP
- */
-
+/****************************************************************************
+ ****************************************************************************
+ ***
+ ***   This header was automatically generated from a Linux kernel header
+ ***   of the same name, to make information necessary for userspace to
+ ***   call into the kernel available to libc.  It contains only constants,
+ ***   structures, and macros generated from the original header, and thus,
+ ***   contains no copyrightable information.
+ ***
+ ***   To edit the content of this header, modify the corresponding
+ ***   source file (e.g. under external/kernel-headers/original/) then
+ ***   run bionic/libc/kernel/tools/update_all.py
+ ***
+ ***   Any manual change here will be lost the next time this script will
+ ***   be run. You've been warned!
+ ***
+ ****************************************************************************
+ ****************************************************************************/
 #ifndef _LINUX_ION_IMX_H
 #define _LINUX_ION_IMX_H
 #include <linux/ion.h>
-
 struct ion_imx_phys_data {
-	int dmafd;
-	unsigned long phys;
+  int dmafd;
+  unsigned long phys;
 };
-
-#define ION_GET_PHYS   _IOWR(ION_IOC_MAGIC, 15, struct ion_phys_data)
-
+#define ION_GET_PHYS _IOWR(ION_IOC_MAGIC, 15, struct ion_imx_phys_data)
 #endif
