af178c7156a1b7c95e44e0c7ea6443b18d8c8842
Fang Hui <hui.fang@nxp.com>
2021-05-12 16:34:51 +0000

    MA-19042 Enable dual basler camera
    . Use imx8mp-evk-dual-basler.dtb as default dtb.
    . start isp media server as "isp_media_server DUAL_CAMERA".
    . Refine camera_config_imx8mp.json to use dual basler cameras.
    
    Change-Id: I534337f2d262064d8421d58bdbe9d346a7ab76a0
    Signed-off-by: Fang Hui <hui.fang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/15878
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- imx8m/evk_8mp/BoardConfig.mk
+++ imx8m/evk_8mp/BoardConfig.mk
@@ -147,8 +147,10 @@ ifeq ($(TARGET_USE_DYNAMIC_PARTITIONS),true)
   ifeq ($(IMX_NO_PRODUCT_PARTITION),true)
     TARGET_BOARD_DTS_CONFIG := imx8mp:imx8mp-evk-no-product.dtb
   else
-    # Default use basler + ov5640
-    TARGET_BOARD_DTS_CONFIG := imx8mp:imx8mp-evk-basler-ov5640.dtb
+    # Default dual basler
+    TARGET_BOARD_DTS_CONFIG := imx8mp:imx8mp-evk-dual-basler.dtb
+    # basler + ov5640
+    TARGET_BOARD_DTS_CONFIG += imx8mp-basler-ov5640:imx8mp-evk-basler-ov5640.dtb
     # Only ov5640
     TARGET_BOARD_DTS_CONFIG += imx8mp-ov5640:imx8mp-evk.dtb
     # Only basler
--- imx8m/evk_8mp/evk_8mp.mk
+++ imx8m/evk_8mp/evk_8mp.mk
@@ -238,6 +238,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(IMX_DEVICE_PATH)/camera_config_imx8mp.json:$(TARGET_COPY_OUT_VENDOR)/etc/configs/camera_config_imx8mp.json \
+    $(IMX_DEVICE_PATH)/camera_config_imx8mp-basler-ov5640.json:$(TARGET_COPY_OUT_VENDOR)/etc/configs/camera_config_imx8mp-basler-ov5640.json \
     $(IMX_DEVICE_PATH)/ov5640_camera_config_imx8mp.json:$(TARGET_COPY_OUT_VENDOR)/etc/configs/ov5640_camera_config_imx8mp.json \
     $(IMX_DEVICE_PATH)/isp_camera_config_imx8mp.json:$(TARGET_COPY_OUT_VENDOR)/etc/configs/isp_camera_config_imx8mp.json \
     $(IMX_DEVICE_PATH)/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
