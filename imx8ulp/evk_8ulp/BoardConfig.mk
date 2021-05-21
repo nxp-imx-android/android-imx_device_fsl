241876c12fc542ea7caf55dcf147c2ce166669b8
Jindong Yue <jindong.yue@nxp.com>
2021-05-21 16:29:54 +0800

    MA-18983 Add initial imx8ulp configuration
    Set ethernet and sensor as y, then
    ./automation_config/automation_config.py automation_config/sample_config_target_nxp_noncommon.yaml
    
    The reference board is evk_8mm.
    
    Change-Id: I501d30ec53f4fd3105632057197b773ae4a9ed5e
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/15940
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>

---
+++ imx8ulp/evk_8ulp/BoardConfig.mk
@@ -0,0 +1,181 @@
+# -------@block_infrastructure-------
+#
+# Product-specific compile-time definitions.
+#
+
+include $(CONFIG_REPO_PATH)/imx8ulp/BoardConfigCommon.mk
+
+# -------@block_common_config-------
+#
+# SoC-specific compile-time definitions.
+#
+
+# value assigned in this part should be fixed for an SoC, right?
+
+BOARD_SOC_TYPE := IMX8ULP
+BOARD_HAVE_VPU := true
+BOARD_VPU_TYPE := hantro
+FSL_VPU_OMX_ONLY := true
+HAVE_FSL_IMX_GPU2D := true
+HAVE_FSL_IMX_GPU3D := true
+HAVE_FSL_IMX_PXP := false
+TARGET_USES_HWC2 := true
+TARGET_HAVE_VULKAN := true
+
+SOONG_CONFIG_IMXPLUGIN += \
+                          BOARD_VPU_TYPE
+
+SOONG_CONFIG_IMXPLUGIN_BOARD_SOC_TYPE = IMX8ULP
+SOONG_CONFIG_IMXPLUGIN_BOARD_HAVE_VPU = true
+SOONG_CONFIG_IMXPLUGIN_BOARD_VPU_TYPE = hantro
+SOONG_CONFIG_IMXPLUGIN_BOARD_VPU_ONLY = false
+
+# -------@block_memory-------
+USE_ION_ALLOCATOR := true
+USE_GPU_ALLOCATOR := false
+
+# -------@block_storage-------
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# use sparse image
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
+
+# Support gpt
+ifeq ($(TARGET_USE_DYNAMIC_PARTITIONS),true)
+  BOARD_BPT_INPUT_FILES += $(CONFIG_REPO_PATH)/common/partition/device-partitions-13GB-ab_super.bpt
+  ADDITION_BPT_PARTITION = partition-table-28GB:$(CONFIG_REPO_PATH)/common/partition/device-partitions-28GB-ab_super.bpt \
+                           partition-table-dual:$(CONFIG_REPO_PATH)/common/partition/device-partitions-13GB-ab-dual-bootloader_super.bpt \
+                           partition-table-28GB-dual:$(CONFIG_REPO_PATH)/common/partition/device-partitions-28GB-ab-dual-bootloader_super.bpt
+else
+  ifeq ($(IMX_NO_PRODUCT_PARTITION),true)
+    BOARD_BPT_INPUT_FILES += $(CONFIG_REPO_PATH)/common/partition/device-partitions-13GB-ab-no-product.bpt
+    ADDITION_BPT_PARTITION = partition-table-28GB:$(CONFIG_REPO_PATH)/common/partition/device-partitions-28GB-ab-no-product.bpt \
+                             partition-table-dual:$(CONFIG_REPO_PATH)/common/partition/device-partitions-13GB-ab-dual-bootloader-no-product.bpt \
+                             partition-table-28GB-dual:$(CONFIG_REPO_PATH)/common/partition/device-partitions-28GB-ab-dual-bootloader-no-product.bpt
+  else
+    BOARD_BPT_INPUT_FILES += $(CONFIG_REPO_PATH)/common/partition/device-partitions-13GB-ab.bpt
+    ADDITION_BPT_PARTITION = partition-table-28GB:$(CONFIG_REPO_PATH)/common/partition/device-partitions-28GB-ab.bpt \
+                             partition-table-dual:$(CONFIG_REPO_PATH)/common/partition/device-partitions-13GB-ab-dual-bootloader.bpt \
+                             partition-table-28GB-dual:$(CONFIG_REPO_PATH)/common/partition/device-partitions-28GB-ab-dual-bootloader.bpt
+  endif
+endif
+
+BOARD_PREBUILT_DTBOIMAGE := $(OUT_DIR)/target/product/$(PRODUCT_DEVICE)/dtbo-imx8mm.img
+
+BOARD_USES_METADATA_PARTITION := true
+BOARD_ROOT_EXTRA_FOLDERS += metadata
+
+# -------@block_security-------
+ENABLE_CFI=false
+
+BOARD_AVB_ENABLE := true
+BOARD_AVB_ALGORITHM := SHA256_RSA4096
+# The testkey_rsa4096.pem is copied from external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_KEY_PATH := $(CONFIG_REPO_PATH)/common/security/testkey_rsa4096.pem
+
+BOARD_AVB_BOOT_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_BOOT_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_BOOT_ROLLBACK_INDEX_LOCATION := 2
+
+# -------@block_treble-------
+# Vendor Interface manifest and compatibility
+DEVICE_MANIFEST_FILE := $(IMX_DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(IMX_DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(IMX_DEVICE_PATH)/device_framework_matrix.xml
+
+
+# -------@block_wifi-------
+# 8mm LPDDR4 board use NXP 8987 wifi
+BOARD_WLAN_DEVICE            := nxp
+WPA_SUPPLICANT_VERSION       := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER  := NL80211
+BOARD_HOSTAPD_DRIVER         := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB               := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+
+# NXP 8987 wifi support dual interface
+WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
+
+# NXP 8987 wifi driver module
+BOARD_VENDOR_KERNEL_MODULES += \
+    $(TARGET_OUT_INTERMEDIATES)/MXMWIFI_OBJ/mlan.ko \
+    $(TARGET_OUT_INTERMEDIATES)/MXMWIFI_OBJ/moal.ko
+
+# -------@block_bluetooth-------
+# NXP 8987 bluetooth
+BOARD_HAVE_BLUETOOTH_NXP := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(IMX_DEVICE_PATH)/bluetooth
+
+# -------@block_sensor-------
+BOARD_USE_SENSOR_FUSION := true
+
+
+# -------@block_kernel_bootimg-------
+BOARD_KERNEL_BASE := 0x40400000
+
+ifeq ($(PRODUCT_IMX_DRM),true)
+CMASIZE=736M
+else
+CMASIZE=800M
+endif
+
+# NXP default config
+BOARD_KERNEL_CMDLINE := init=/init androidboot.console=ttymxc1 androidboot.hardware=nxp firmware_class.path=/vendor/firmware loop.max_part=7
+
+# memory config
+BOARD_KERNEL_CMDLINE += transparent_hugepage=never
+
+# display config
+BOARD_KERNEL_CMDLINE += androidboot.lcd_density=240 androidboot.primary_display=imx-drm
+
+# wifi config
+BOARD_KERNEL_CMDLINE += androidboot.wificountrycode=CN moal.mod_para=wifi_mod_para_sd8987.conf
+
+# low memory device build config
+ifeq ($(LOW_MEMORY),true)
+BOARD_KERNEL_CMDLINE += cma=320M@0x400M-0xb80M androidboot.displaymode=720p galcore.contiguousSize=33554432
+else
+BOARD_KERNEL_CMDLINE += cma=$(CMASIZE)@0x400M-0xb80M
+endif
+
+ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
+BOARD_KERNEL_CMDLINE += androidboot.vendor.sysrq=1
+endif
+
+ifeq ($(TARGET_USE_DYNAMIC_PARTITIONS),true)
+  # dts target for imx8mm_evk with DDR4 on board
+  ifeq ($(IMX_NO_PRODUCT_PARTITION),true)
+    # dts without product partition
+    TARGET_BOARD_DTS_CONFIG ?= imx8mm:imx8mm-evk-no-product.dtb
+    TARGET_BOARD_DTS_CONFIG += imx8mm-ddr4:imx8mm-ddr4-evk-no-product.dtb
+  else
+    TARGET_BOARD_DTS_CONFIG ?= imx8mm-ddr4:imx8mm-ddr4-evk.dtb
+    # imx8mm with MIPI-HDMI display and NXP wifi
+    TARGET_BOARD_DTS_CONFIG += imx8mm:imx8mm-evk.dtb
+    # imx8mm with MIPI panel display and NXP wifi
+    TARGET_BOARD_DTS_CONFIG += imx8mm-mipi-panel:imx8mm-evk-rm67191.dtb
+    # imx8mm with MIPI-HDMI display, NXP wifi and m4 image to support LPA
+    TARGET_BOARD_DTS_CONFIG += imx8mm-m4:imx8mm-evk-rpmsg.dtb
+  endif
+else
+  ifeq ($(IMX_NO_PRODUCT_PARTITION),true)
+    TARGET_BOARD_DTS_CONFIG ?= imx8mm:imx8mm-evk-no-product-no-dynamic_partition.dtb
+    TARGET_BOARD_DTS_CONFIG += imx8mm-ddr4:imx8mm-ddr4-evk-no-product-no-dynamic_partition.dtb
+  else
+    TARGET_BOARD_DTS_CONFIG ?= imx8mm:imx8mm-evk-no-dynamic_partition.dtb
+    TARGET_BOARD_DTS_CONFIG += imx8mm-ddr4:imx8mm-ddr4-evk-no-dynamic_partition.dtb
+  endif
+endif
+
+ALL_DEFAULT_INSTALLED_MODULES += $(BOARD_VENDOR_KERNEL_MODULES)
+
+# -------@block_sepolicy-------
+BOARD_SEPOLICY_DIRS := \
+       $(CONFIG_REPO_PATH)/imx8ulp/sepolicy \
+       $(IMX_DEVICE_PATH)/sepolicy
+
+ifeq ($(PRODUCT_IMX_DRM),true)
+BOARD_SEPOLICY_DIRS += \
+       $(IMX_DEVICE_PATH)/sepolicy_drm
+endif
+
