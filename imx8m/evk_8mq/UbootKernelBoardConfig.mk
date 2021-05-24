b00bb54ea426344d454d156850a217f13908431f
zhipeng.wang <zhipeng.wang_1@nxp.com>
2021-05-24 11:01:05 +0800

    MA-19114 Support GKI for 8mq
    Add driver modules to support GKI on 8mq.
    
    Change-Id: Ic933fa77f3021b127d4fc2b21944eb5bd9a44bca
    Signed-off-by: zhipeng.wang <zhipeng.wang_1@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/16007
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>

--- imx8m/evk_8mq/UbootKernelBoardConfig.mk
+++ imx8m/evk_8mq/UbootKernelBoardConfig.mk
@@ -13,7 +13,12 @@ endif
 # u-boot target used by uuu for imx8mq_evk
 TARGET_BOOTLOADER_CONFIG += imx8mq-evk-uuu:imx8mq_evk_android_uuu_defconfig
 
+ifeq ($(IMX8MQ_USES_GKI),true)
+TARGET_KERNEL_DEFCONFIG := gki_defconfig
+TARGET_KERNEL_GKI_DEFCONF:= imx8mq_gki.fragment
+else
 TARGET_KERNEL_DEFCONFIG := imx_v8_android_defconfig
+endif
 # TARGET_KERNEL_ADDITION_DEFCONF ?= android_addition_defconfig
 
 
