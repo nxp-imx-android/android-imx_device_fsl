cd3b5f53803de06441f5e9c0babe8b2e7e361842
Xu Mao <mao.xu@nxp.com>
2021-06-07 16:31:14 +0800

    MA-19141 android 11 enable 8ulp codec
    Change-Id: I2f01b5927b7ed8f5e43ce68c20d533aae7b2e222
    Signed-off-by: Xu Mao <mao.xu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/16070
    Reviewed-by: xiahong bao <xiahong.bao@nxp.com>

--- imx8ulp/evk_8ulp/AndroidBoard.mk
+++ imx8ulp/evk_8ulp/AndroidBoard.mk
@@ -4,3 +4,4 @@ include $(CONFIG_REPO_PATH)/common/build/dtbo.mk
 include $(CONFIG_REPO_PATH)/common/build/imx-recovery.mk
 include $(CONFIG_REPO_PATH)/common/build/gpt.mk
 include $(FSL_PROPRIETARY_PATH)/fsl-proprietary/sensor/fsl-sensor.mk
+-include $(IMX_MEDIA_CODEC_XML_PATH)/mediacodec-profile/mediacodec-profile.mk
