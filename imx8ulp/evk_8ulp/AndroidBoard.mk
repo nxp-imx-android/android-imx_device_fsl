57edf26db88297e5a0a47d365decb4f7be405268
Fang Hui <hui.fang@nxp.com>
2021-06-08 13:53:52 +0000

    MA-19140 camera record failed on 8ulp
    Add build for media profile
    
    Change-Id: I74015ffecb35ae2d38f6cc38da20265fdcdafd3b
    Signed-off-by: Fang Hui <hui.fang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/16080
    Reviewed-by: Tian Yang <yang.tian@nxp.com>

--- imx8ulp/evk_8ulp/AndroidBoard.mk
+++ imx8ulp/evk_8ulp/AndroidBoard.mk
@@ -4,4 +4,5 @@ include $(CONFIG_REPO_PATH)/common/build/dtbo.mk
 include $(CONFIG_REPO_PATH)/common/build/imx-recovery.mk
 include $(CONFIG_REPO_PATH)/common/build/gpt.mk
 include $(FSL_PROPRIETARY_PATH)/fsl-proprietary/sensor/fsl-sensor.mk
+include $(FSL_PROPRIETARY_PATH)/fsl-proprietary/media-profile/media-profile.mk
 -include $(IMX_MEDIA_CODEC_XML_PATH)/mediacodec-profile/mediacodec-profile.mk
