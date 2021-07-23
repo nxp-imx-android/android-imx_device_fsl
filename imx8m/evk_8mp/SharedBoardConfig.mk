31527d8cecad6ad33ce4b2e23ca229d9aa4135f5
Jindong Yue <jindong.yue@nxp.com>
2021-07-23 14:19:41 +0800

    MA-19293 Fix rpmsg audio not work issue after merge kernel
    Fix rpmsg audio driver module name as snd-soc-fsl-rpmsg.ko
    and imx-audio-rpmsg.ko
    Also fix rpmsg extcon node path.
    
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Change-Id: Iacc63e1294d1510d3f4fe3916cef5a37ed6499b6
    Reviewed-on: http://androidsource.ap.freescale.net/project/16708
    Reviewed-by: zhipeng.wang <zhipeng.wang_1@nxp.com>

--- imx8m/evk_8mp/SharedBoardConfig.mk
+++ imx8m/evk_8mp/SharedBoardConfig.mk
@@ -60,8 +60,8 @@ BOARD_VENDOR_KERNEL_MODULES += \
     $(KERNEL_OUT)/drivers/remoteproc/imx_rproc.ko \
     $(KERNEL_OUT)/drivers/i2c/busses/i2c-rpmsg-imx.ko \
     $(KERNEL_OUT)/sound/soc/fsl/imx-pcm-rpmsg.ko \
-    $(KERNEL_OUT)/sound/soc/fsl/snd-soc-fsl-rpmsg-i2s.ko \
-    $(KERNEL_OUT)/sound/soc/fsl/imx-i2s-rpmsg.ko \
+    $(KERNEL_OUT)/sound/soc/fsl/snd-soc-fsl-rpmsg.ko \
+    $(KERNEL_OUT)/sound/soc/fsl/imx-audio-rpmsg.ko \
     $(KERNEL_OUT)/sound/soc/codecs/snd-soc-rpmsg-wm8960.ko \
     $(KERNEL_OUT)/sound/soc/codecs/snd-soc-rpmsg-wm8960-i2c.ko \
     $(KERNEL_OUT)/sound/soc/fsl/snd-soc-imx-rpmsg.ko \
