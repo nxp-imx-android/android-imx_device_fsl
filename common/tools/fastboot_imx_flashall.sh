61a3852fa25124cad664c2571caf16a69411c11e
Jessie Hao <juan.hao@nxp.com>
2021-08-18 10:34:56 +0800

    MA-19338 Add HDMI display support for evk_8ulp
    evk_8ulp support HDMI display, add the corresponding dts file.
    And update uuu/fastboot scripts.
    
    Change-Id: Ibc1a5103ff0b9becb636a957813ce0dec2abb98f
    Signed-off-by: Jessie Hao <juan.hao@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/16839
    Reviewed-by: faqiang.zhu <faqiang.zhu@nxp.com>
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>

--- common/tools/fastboot_imx_flashall.sh
+++ common/tools/fastboot_imx_flashall.sh
@@ -68,6 +68,8 @@ options:
                            ├────────────────┼──────────────────────────────────────────────────────────────────────────────────────────────────────┤
                            │   imx8qm       │  hdmi mipi-panel md xen esai                                                                         │
                            ├────────────────┼──────────────────────────────────────────────────────────────────────────────────────────────────────┤
+                           │   imx8ulp      │  hdmi                                                                                                │
+                           ├────────────────┼──────────────────────────────────────────────────────────────────────────────────────────────────────┤
                            │   imx7ulp      │  evk-mipi evk mipi                                                                                   │
                            └────────────────┴──────────────────────────────────────────────────────────────────────────────────────────────────────┘
 
@@ -333,6 +335,7 @@ imx8mq_dtb_feature=(dual mipi-panel mipi)
 imx8mp_dtb_feature=(rpmsg lvds-panel lvds mipi-panel basler powersave powersave-non-rpmsg basler-ov5640 ov5640)
 imx8qxp_dtb_feature=()
 imx8qm_dtb_feature=(hdmi hdmi-rx mipi-panel md xen esai)
+imx8ulp_dtb_feature=(hdmi)
 imx7ulp_dtb_feature=(evk-mipi evk mipi)
 
 # an array to collect the supported soc_names
