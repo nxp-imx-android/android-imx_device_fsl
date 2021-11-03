9d73c758e382a28cf614bf1f585611c2fa452d05
zhang sanshan <pete.zhang@nxp.com>
2021-11-03 14:21:15 +0800

    MA-19678-1 [Android] change wifi to IW416 for imx8ulp
    wifi/bt can't work in most time on imx8ulp  and suspend/resume also
    can't work because wifi module failed to suspend for android12.
    8987 is not supported on im8ulp and will convert to IW416 for imx8ulp beta release.
    
    Change-Id: If34f39216a66b8ea75e54d8c38dbe1fde9f34f63
    Signed-off-by: zhang sanshan <pete.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/17504
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>

--- imx8ulp/evk_8ulp/BoardConfig.mk
+++ imx8ulp/evk_8ulp/BoardConfig.mk
@@ -125,7 +125,7 @@ BOARD_BOOTCONFIG += androidboot.lcd_density=240 androidboot.primary_display=imx-
 
 # wifi config
 BOARD_BOOTCONFIG += androidboot.wificountrycode=CN
-BOARD_KERNEL_CMDLINE += moal.mod_para=wifi_mod_para_sd8987.conf
+BOARD_KERNEL_CMDLINE += moal.mod_para=wifi_mod_para_sd416.conf
 
 # low memory device build config
 ifeq ($(LOW_MEMORY),true)
