9e0c5c20ee7a960e926f9a5aee46fadddf6e39d5
Jindong Yue <jindong.yue@nxp.com>
2022-02-11 15:01:13 +0800

    MA-19325-6 imx-make.sh: exit the script if failed build android
    Exit the script if meet build error.
    
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Change-Id: I6f87b7fb9bfec595d1444924f51551d407a648df
    Reviewed-on: http://androidsource.ap.freescale.net/project/18134
    Reviewed-by: faqiang.zhu <faqiang.zhu@nxp.com>

--- common/tools/imx-make.sh
+++ common/tools/imx-make.sh
@@ -187,7 +187,7 @@ if [ ${build_android_flag} -eq 1 ] || [ ${build_whole_android_flag} -eq 1 ]; the
     if [ -n "${build_bootimage}" ] || [ ${build_whole_android_flag} -eq 1 ]; then
         rm -rf ${OUT}/boot.img
     fi
-    TARGET_IMX_KERNEL=true make ${parallel_option} ${build_bootimage} ${build_vendorbootimage} ${build_dtboimage} ${build_vendorimage}
+    TARGET_IMX_KERNEL=true make ${parallel_option} ${build_bootimage} ${build_vendorbootimage} ${build_dtboimage} ${build_vendorimage} || exit
     if [ -n "${build_bootimage}" ] || [ ${build_whole_android_flag} -eq 1 ]; then
         if [ ${TARGET_PRODUCT} = "evk_8mp" ] || [ ${TARGET_PRODUCT} = "evk_8mn" ] \
         || [ ${TARGET_PRODUCT} = "evk_8ulp" ] \
