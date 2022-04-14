592f1316a064f602d2983cd5f84b460d40072adf
Zhipeng Wang <zhipeng.wang_1@nxp.com>
2022-04-14 13:52:49 +0800

    MA-20138-26 imx-make.sh: support GKI boot image
    Revert "MA-20005-3 imx-make.sh: Do not build GKI boot image"
    
    This reverts commit 1c6f50b1002e9bc7b5c821947e83330e91f0be81.
    
    Change-Id: I63be125ca53536e771841996a2979272db190b0c
    Reviewed-on: http://androidsource.ap.freescale.net/project/18683
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>

--- common/tools/imx-make.sh
+++ common/tools/imx-make.sh
@@ -188,6 +188,15 @@ if [ ${build_android_flag} -eq 1 ] || [ ${build_whole_android_flag} -eq 1 ]; the
         rm -rf ${OUT}/boot.img
     fi
     TARGET_IMX_KERNEL=true make ${parallel_option} ${build_bootimage} ${build_vendorbootimage} ${build_dtboimage} ${build_vendorimage} || exit
+    if [ -n "${build_bootimage}" ] || [ ${build_whole_android_flag} -eq 1 ]; then
+        if [ ${TARGET_PRODUCT} = "evk_8mp" ] || [ ${TARGET_PRODUCT} = "evk_8mn" ] \
+        || [ ${TARGET_PRODUCT} = "evk_8ulp" ] \
+        || [ ${TARGET_PRODUCT} = "evk_8mm" ] || [ ${TARGET_PRODUCT} = "evk_8mq" ]; then
+            mv ${OUT}/boot.img ${OUT}/boot-imx.img
+            # sign prebuilt gki boot.img
+            make bootimage
+        fi
+    fi
 fi
 
 # copy the uboot output to ${OUT_DIR}
