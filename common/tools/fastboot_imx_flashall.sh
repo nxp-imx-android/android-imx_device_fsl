061859db72f4558823a766e9b56acdbd6f4a3ee0
Jindong Yue <jindong.yue@nxp.com>
2022-05-24 14:58:12 +0800

    MA-20170-4 SOF: Add missing sof dtbo target for 8qxp and 8mp
    Add sof dtbo target for 8qxp.
    Add missing sof in .bat files for 8mp.
    
    Change-Id: Iabd4e07e758bd3746dce204b658fa6cb3751f98b
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/19044
    Reviewed-by: faqiang.zhu <faqiang.zhu@nxp.com>

--- common/tools/fastboot_imx_flashall.sh
+++ common/tools/fastboot_imx_flashall.sh
@@ -66,7 +66,7 @@ options:
                            │   imx8mp       │  rpmsg lvds-panel lvds mipi-panel mipi-panel-rm67191 basler powersave powersave-non-rpmsg            │
                            │                │  basler-ov5640 ov5640 sof dual-os08a20 os08a20-ov5640 os08a20                                        │
                            ├────────────────┼──────────────────────────────────────────────────────────────────────────────────────────────────────┤
-                           │   imx8qxp      │                                                                                                      │
+                           │   imx8qxp      │  sof                                                                                                 │
                            ├────────────────┼──────────────────────────────────────────────────────────────────────────────────────────────────────┤
                            │   imx8qm       │  hdmi mipi-panel mipi-panel-rm67191 md xen esai sof                                                  │
                            ├────────────────┼──────────────────────────────────────────────────────────────────────────────────────────────────────┤
@@ -345,7 +345,7 @@ imx8mm_dtb_feature=(ddr4 m4 mipi-panel mipi-panel-rm67191)
 imx8mn_dtb_feature=(mipi-panel mipi-panel-rm67191 rpmsg ddr4 ddr4-mipi-panel ddr4-mipi-panel-rm67191 ddr4-rpmsg)
 imx8mq_dtb_feature=(dual mipi-panel mipi-panel-rm67191 mipi)
 imx8mp_dtb_feature=(rpmsg lvds-panel lvds mipi-panel mipi-panel-rm67191 basler powersave powersave-non-rpmsg basler-ov5640 ov5640 sof dual-os08a20 os08a20-ov5640 os08a20)
-imx8qxp_dtb_feature=()
+imx8qxp_dtb_feature=(sof)
 imx8qm_dtb_feature=(hdmi hdmi-rx mipi-panel mipi-panel-rm67191 md xen esai sof)
 imx8ulp_dtb_feature=(hdmi epdc 9x9 9x9-hdmi sof)
 imx7ulp_dtb_feature=(evk-mipi evk mipi)
