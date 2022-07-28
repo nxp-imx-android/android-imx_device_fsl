6b44d77077be069a084df5b4c97721e54917267f
Zhang Bo <bo.zhang@nxp.com>
2022-07-28 17:21:53 +0800

    MA-20481 Add initial configuration for imx8ulp watch board
    Set ethernet and camera as n, set sensor to y, then
    ./automation_config/automation_config.py automation_config/sample_config_target_nxp_noncommon.yaml
    
    Add some .bpt files for 7GB eMMC board. Remove unused uboot build and dtbo image build config.
    The reference board is evk_8ulp.
    
    Change-Id: I624b4e3e2f40b8ac258553267595c3697a69b818
    Signed-off-by: Zhang Bo <bo.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/19440
    Reviewed-by: faqiang.zhu <faqiang.zhu@nxp.com>
    (cherry picked from commit e2ec432452548148940fc972de9ab75349b4442e)
    Reviewed-on: http://androidsource.ap.freescale.net/project/19469
    Reviewed-by: ji.luo <ji.luo@nxp.com>

---
+++ imx8ulp/watch_8ulp/UbootKernelBoardConfig.mk
@@ -0,0 +1,24 @@
+TARGET_BOOTLOADER_POSTFIX := bin
+UBOOT_POST_PROCESS := true
+
+# u-boot target for imx8ulp_watch board
+TARGET_BOOTLOADER_CONFIG := imx8ulp:imx8ulp_watch_android_defconfig
+TARGET_BOOTLOADER_CONFIG += imx8ulp-dual:imx8ulp_watch_android_dual_defconfig
+TARGET_BOOTLOADER_CONFIG += imx8ulp-trusty-dual:imx8ulp_watch_android_trusty_dual_defconfig
+TARGET_BOOTLOADER_CONFIG += imx8ulp-evk-uuu:imx8ulp_watch_android_uuu_defconfig
+
+# imx8ulp kernel defconfig
+ifeq ($(IMX8ULP_USES_GKI),true)
+TARGET_KERNEL_DEFCONFIG := gki_defconfig
+TARGET_KERNEL_GKI_DEFCONF:= imx8ulp_gki.fragment
+else
+TARGET_KERNEL_DEFCONFIG := imx_v8_android_defconfig
+endif
+TARGET_KERNEL_ADDITION_DEFCONF := android_addition_defconfig
+
+# absolute path is used, not the same as relative path used in AOSP make
+TARGET_DEVICE_DIR := $(patsubst %/, %, $(dir $(realpath $(lastword $(MAKEFILE_LIST)))))
+
+# define bootloader rollback index
+BOOTLOADER_RBINDEX ?= 0
+
