68997b859f20e91269df2a79fb72f6a0f70e1224
Zhang Bo <bo.zhang@nxp.com>
2022-09-06 18:32:53 +0800

    MA-20619 Enable light sensor TSL2540 for watch board
    New driver module added for light sensor tsl2540
    
    Change-Id: I47652cdd9d553514061203ed1b5aeab602182bb9
    Signed-off-by: Zhang Bo <bo.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/19836
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>

--- imx8ulp/watch_8ulp/SharedBoardConfig.mk
+++ imx8ulp/watch_8ulp/SharedBoardConfig.mk
@@ -225,6 +225,7 @@ BOARD_VENDOR_KERNEL_MODULES += \
     $(KERNEL_OUT)/drivers/iio/pressure/mpl3115.ko \
     $(KERNEL_OUT)/drivers/iio/imu/rpmsg_iio_pedometer.ko \
     $(KERNEL_OUT)/drivers/iio/health/max30102.ko \
+    $(KERNEL_OUT)/drivers/iio/light/tsl2540.ko \
     $(KERNEL_OUT)/drivers/mtd/mtd.ko \
     $(KERNEL_OUT)/drivers/mtd/chips/chipreg.ko \
     $(KERNEL_OUT)/drivers/mtd/parsers/ofpart.ko \
