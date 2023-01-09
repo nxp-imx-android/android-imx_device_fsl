aa39799e5d0ca1c673796967d08394d4e45301a8
Jindong Yue <jindong.yue@nxp.com>
2023-01-09 15:27:32 +0800

    MA-20990-1 Add initial imx93 configuration
    ./automation_config/automation_config.py automation_config/sample_config_target_nxp_noncommon.yaml
    
    The reference product is evk_8ulp.
    
    Change-Id: I7721a2ac1c79287b2d042f509198f32bfb972584
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/20406
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>

---
+++ imx9/evk_93/UbootKernelBoardConfig.mk
@@ -0,0 +1,29 @@
+TARGET_BOOTLOADER_POSTFIX := bin
+UBOOT_POST_PROCESS := true
+
+# u-boot target for imx8ulp_evk board
+TARGET_BOOTLOADER_CONFIG := imx8ulp:imx8ulp_evk_android_defconfig
+TARGET_BOOTLOADER_CONFIG += imx8ulp-trusty-secure-unlock-dual:imx8ulp_evk_android_trusty_secure_unlock_dual_defconfig
+TARGET_BOOTLOADER_CONFIG += imx8ulp-dual:imx8ulp_evk_android_dual_defconfig
+TARGET_BOOTLOADER_CONFIG += imx8ulp-trusty-dual:imx8ulp_evk_android_trusty_dual_defconfig
+TARGET_BOOTLOADER_CONFIG += imx8ulp-trusty-lpa-dual:imx8ulp_evk_android_trusty_dual_defconfig
+TARGET_BOOTLOADER_CONFIG += imx8ulp-evk-uuu:imx8ulp_evk_android_uuu_defconfig
+TARGET_BOOTLOADER_CONFIG += imx8ulp-9x9:imx8ulp_9x9_evk_android_defconfig
+TARGET_BOOTLOADER_CONFIG += imx8ulp-trusty-9x9-dual:imx8ulp_9x9_evk_android_trusty_dual_defconfig
+TARGET_BOOTLOADER_CONFIG += imx8ulp-9x9-evk-uuu:imx8ulp_9x9_evk_android_uuu_defconfig
+
+# imx8ulp kernel defconfig
+ifeq ($(IMX8ULP_USES_GKI),true)
+TARGET_KERNEL_DEFCONFIG := gki_defconfig
+TARGET_KERNEL_GKI_DEFCONF:= imx8ulp_gki.fragment
+else
+TARGET_KERNEL_DEFCONFIG := imx_v8_android_defconfig
+endif
+TARGET_KERNEL_ADDITION_DEFCONF := android_addition_defconfig
+
+# absolute path is used, not the same as relative path used in AOSP make
+TARGET_DEVICE_DIR := $(patsubst %/, %, $(dir $(realpath $(lastword $(MAKEFILE_LIST)))))
+
+# define bootloader rollback index
+BOOTLOADER_RBINDEX ?= 0
+
