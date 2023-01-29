3ecfb4c2260c33ef95b11f43b09659b1983bdbe8
Maximus Sun <maximus.sun@nxp.com>
2023-01-29 16:18:19 +0800

    MA-21008 imx93: Add dual bootloader feature for iMX93_EVK
    Add configuration: imx93_11x11_evk_android_dual_defconfig
    
    Change-Id: I8c5b0731a02a5d48a0d1869b454cc48d1ebff887
    Signed-off-by: Maximus Sun <maximus.sun@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/20530
    Reviewed-by: ji.luo <ji.luo@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/20807

--- imx9/evk_93/UbootKernelBoardConfig.mk
+++ imx9/evk_93/UbootKernelBoardConfig.mk
@@ -3,6 +3,7 @@ UBOOT_POST_PROCESS := true
 
 # u-boot target for imx93_evk board
 TARGET_BOOTLOADER_CONFIG := imx93:imx93_11x11_evk_android_defconfig
+TARGET_BOOTLOADER_CONFIG += imx93-dual:imx93_11x11_evk_android_dual_defconfig
 TARGET_BOOTLOADER_CONFIG += imx93-evk-uuu:imx93_11x11_evk_android_uuu_defconfig
 
 # imx93 kernel defconfig
