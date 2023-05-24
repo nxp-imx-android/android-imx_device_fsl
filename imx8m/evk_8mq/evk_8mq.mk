0ec59f3de8b0db3c14efd7595b5edf7293c29ef5
Jindong Yue <jindong.yue@nxp.com>
2023-05-24 14:48:20 +0900

    MA-20107-16 Put regulatory.db firmware on vendor
    Kernel loadable module "cfg80211" is now on vendor_dlkm,
    so put "regulatory.db" firmware on vendor also.
    
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Change-Id: I306c36a6e534e2af05dc57915bf66f7ce3d1be75
    Reviewed-on: http://androidsource.nxp.com/project/21785
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>
    (cherry picked from commit 4d8611ce68ecfe8c6081a935c5a915c8811fbe64)
    Reviewed-on: http://androidsource.nxp.com/project/21796

--- imx8m/evk_8mq/evk_8mq.mk
+++ imx8m/evk_8mq/evk_8mq.mk
@@ -344,8 +344,8 @@ PRODUCT_COPY_FILES += \
 
 # Wifi regulatory
 PRODUCT_COPY_FILES += \
-    external/wireless-regdb/regulatory.db:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/firmware/regulatory.db \
-    external/wireless-regdb/regulatory.db.p7s:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/firmware/regulatory.db.p7s
+    external/wireless-regdb/regulatory.db:$(TARGET_COPY_OUT_VENDOR)/firmware/regulatory.db \
+    external/wireless-regdb/regulatory.db.p7s:$(TARGET_COPY_OUT_VENDOR)/firmware/regulatory.db.p7s
 
 # -------@block_bluetooth-------
 # Bluetooth HAL
