030e839db84af21d2f5f275485b6cee726569180
Ji Luo <ji.luo@nxp.com>
2023-07-11 18:36:49 +0900

    MA-21456-2 support imx8mq wevk flash
    add imx8mq 'wevk' dtbo flash target.
    
    Change-Id: Ic5aeb60025454072e6495d17bc91e4ce2b0b206b
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/22186
    Reviewed-by: Faqiang Zhu <faqiang.zhu@nxp.com>

--- common/tools/uuu_imx_android_flash.sh
+++ common/tools/uuu_imx_android_flash.sh
@@ -64,7 +64,7 @@ options:
                            ├────────────────┼──────────────────────────────────────────────────────────────────────────────────────────────────────┤
                            │   imx8mn       │  mipi-panel mipi-panel-rm67191 rpmsg ddr4 ddr4-mipi-panel mipi-panel-rm67191 ddr4-rpmsg              │
                            ├────────────────┼──────────────────────────────────────────────────────────────────────────────────────────────────────┤
-                           │   imx8mq       │  dual mipi-panel mipi-panel-rm67191                                                                  │
+                           │   imx8mq       │  wevk dual mipi-panel mipi-panel-rm67191                                                             │
                            ├────────────────┼──────────────────────────────────────────────────────────────────────────────────────────────────────┤
                            │   imx8mp       │  rpmsg lvds-panel lvds mipi-panel mipi-panel-rm67191 basler powersave powersave-non-rpmsg            │
                            │                │  basler-ov5640 ov5640.img sof dual-basler os08a20-ov5640 os08a20                                     │
@@ -464,7 +464,7 @@ imx93_uboot_feature=(dual trusty-dual evk-uuu)
 
 imx8mm_dtb_feature=(ddr4 m4 mipi-panel mipi-panel-rm67191)
 imx8mn_dtb_feature=(mipi-panel mipi-panel-rm67191 rpmsg ddr4 ddr4-mipi-panel ddr4-mipi-panel-rm67191 ddr4-rpmsg)
-imx8mq_dtb_feature=(dual mipi-panel mipi-panel-rm67191 mipi)
+imx8mq_dtb_feature=(wevk dual mipi-panel mipi-panel-rm67191 mipi)
 imx8mp_dtb_feature=(rpmsg lvds-panel lvds mipi-panel mipi-panel-rm67191 basler powersave powersave-non-rpmsg basler-ov5640 ov5640 sof dual-basler os08a20-ov5640 os08a20 revb4 rpmsg-revb4 lvds-panel-revb4 lvds-revb4 mipi-panel-revb4 mipi-panel-rm67191-revb4 basler-revb4 powersave-revb4 powersave-non-rpmsg-revb4 basler-ov5640-revb4 ov5640-revb4 sof-revb4 dual-basler-revb4 os08a20-ov5640-revb4 os08a20-revb4)
 imx8qxp_dtb_feature=(sof)
 imx8qm_dtb_feature=(hdmi hdmi-rx mipi-panel mipi-panel-rm67191 md xen esai sof)
