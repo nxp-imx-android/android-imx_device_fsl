de8738293975a9cd9c7c0b4a165987a02ef6f8f1
Ming Zhou <ming.zhou@nxp.com>
2023-07-19 11:16:23 +0800

    MA-21484 add opencl convert library and kernel file
    Signed-off-by: Ming Zhou <ming.zhou@nxp.com>
    Change-Id: If0f925832f58ebbbf68276f4e0309218ab87b737
    Reviewed-on: http://androidsource.nxp.com/project/22306
    Reviewed-by: X.H. Bao <xiahong.bao@nxp.com>

--- imx8q/ProductConfigCommon.mk
+++ imx8q/ProductConfigCommon.mk
@@ -139,8 +139,8 @@ PRODUCT_PACKAGES += \
     lib_imx_c2_process_isi_pre \
     lib_imx_c2_process_g2d_post \
     lib_imx_c2_g2d_filter \
-    lib_imx_opencl_process \
-    opencl_process.cl \
+    lib_imx_opencl_converter \
+    ocl_converter.cl \
     lib_imx_c2_opencl_filter \
     lib_imx_c2_isi_filter \
     lib_imx_c2_opencl_pre_filter \
