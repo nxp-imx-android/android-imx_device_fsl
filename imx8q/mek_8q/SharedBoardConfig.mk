d0954b19a5fa0209b2230d60f11e964b77430f60
Jindong Yue <jindong.yue@nxp.com>
2023-07-27 17:13:35 +0800

    MA-21459 Add i2c mux gpio driver for 8q
    Add i2c mux gpio driver to fix no wm8960 card issue after
    kernel commit merged:
    82099891b9b7 LF-9482-2: arm64: config: add sensor support for imx8qxp WCPU boards
    
    Change-Id: Ib4a0bfc7a18eb7e8f98f2cf24a249a6230bd6b50
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/22419
    Reviewed-by: Zhipeng Wang <zhipeng.wang_1@nxp.com>
    (cherry picked from commit 84e664269524257257847d059cf4669b556f8d3c)
    Reviewed-on: http://androidsource.nxp.com/project/22423

--- imx8q/mek_8q/SharedBoardConfig.mk
+++ imx8q/mek_8q/SharedBoardConfig.mk
@@ -103,6 +103,8 @@ BOARD_VENDOR_RAMDISK_KERNEL_MODULES += \
     $(KERNEL_OUT)/drivers/video/backlight/pwm_bl.ko \
     $(KERNEL_OUT)/drivers/i2c/busses/i2c-rpmsg-imx.ko \
     $(KERNEL_OUT)/drivers/i2c/busses/i2c-imx-lpi2c.ko \
+    $(KERNEL_OUT)/drivers/i2c/i2c-mux.ko \
+    $(KERNEL_OUT)/drivers/i2c/muxes/i2c-mux-gpio.ko \
     $(KERNEL_OUT)/drivers/spi/spi-fsl-lpspi.ko \
     $(KERNEL_OUT)/drivers/spi/spi-nxp-fspi.ko \
     $(KERNEL_OUT)/drivers/iio/buffer/kfifo_buf.ko \
