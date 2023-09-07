c94213ca10ceabfd96d7e4739070089397b64a09
Jessie Hao <juan.hao@nxp.com>
2023-09-07 18:40:25 +0800

    MA-21637 Declare android.hardware.vulkan.version to 1.3
    The "android.hardware.vulkan.version" should update to version-1_3
    after GPU vulkan driver export vulkan 1.3 API.
    
    Test:
    CtsGraphicsTestCases
    android.graphics.cts.VulkanFeaturesTest#testVulkanHardwareFeatures
    
    Change-Id: I28579f93307f787e89361552549049dd9b6e5e94
    Signed-off-by: Jessie Hao <juan.hao@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/22669
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>

--- imx8ulp/watch_8ulp/watch_8ulp.mk
+++ imx8ulp/watch_8ulp/watch_8ulp.mk
@@ -469,7 +469,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-0.xml \
-    frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_3.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2023-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2023-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
